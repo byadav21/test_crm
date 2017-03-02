a5e0687955c6a6a0cdf2d90cc66c8728f1e0ba41
Ravi Tiwari <ravibhushanblevel@gmail.com>
2017-03-02 02:31:30 -0500

    Update logic_hooks.php

--- custom/modules/te_student_batch/logic_hooks.php
+++ custom/modules/te_student_batch/logic_hooks.php
@@ -6,5 +6,9 @@ $hook_array = Array();
 $hook_array['after_save'] = Array(); 
 $hook_array['after_save'][] = Array(1, 'add payment plan details', 'custom/modules/te_student_batch/addStudentPayment.php','addStudentPaymentClass', 'makePayment'); 
 $hook_array['process_record'][] = Array(3, 'List_view student fields', 'custom/modules/te_student_batch/listview_display.php','listviewClass', 'listview'); 
+
+$hook_array['process_record'] = Array();
+$hook_array['process_record'][] = Array(1,'Approve Dropout Request','custom/modules/te_student_batch/dropout_hook.php','DropoutRequest','approveDropoutRequest');
+
 ?>
 	
