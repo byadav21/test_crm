8312aa501cbe75de97581e7eac7c734829759c8a
Ravi Tiwari <ravibhushanblevel@gmail.com>
2017-03-02 06:30:35 -0500

    Update dropout_hook.php

--- custom/modules/te_student_batch/dropout_hook.php
+++ custom/modules/te_student_batch/dropout_hook.php
@@ -2,7 +2,7 @@
 if (!defined('sugarEntry') || !sugarEntry) die('Not A Valid Entry Point');
 class DropoutRequest{	
 	function approveDropoutRequest($bean, $event, $argument){		
-		if($bean->dropout_status=="Pending"){
+		if($bean->dropout_status=="Pending" && $current_user->designation=="BUH"){
 			$dropout_action_list =$GLOBALS['app_list_strings']['dropuout_status_list'];
 			$action="<span id='dropout_request_".$bean->id."'></span><select name='dropout_status' id='".$bean->id."' onchange='return changeDropoutStatus(this.id,this.value);' style='width:113PX !IMPORTANT'><option value=''>--select--</option>";
 			foreach($dropout_action_list as $key=>$value){
