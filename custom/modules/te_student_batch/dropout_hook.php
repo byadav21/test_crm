253106d84ab64dc9cd134757f4bd2d951215a492
Ravi Tiwari <ravibhushanblevel@gmail.com>
2017-03-02 02:32:09 -0500

    Upload new file

---
+++ custom/modules/te_student_batch/dropout_hook.php
@@ -0,0 +1,18 @@
+<?php
+if (!defined('sugarEntry') || !sugarEntry) die('Not A Valid Entry Point');
+class DropoutRequest{	
+	function approveDropoutRequest($bean, $event, $argument){		
+		if($bean->dropout_status=="Pending"){
+			$dropout_action_list =$GLOBALS['app_list_strings']['dropuout_status_list'];
+			$action="<span id='dropout_request_".$bean->id."'></span><select name='dropout_status' id='".$bean->id."' onchange='return changeDropoutStatus(this.id,this.value);' style='width:113PX !IMPORTANT'><option value=''>--select--</option>";
+			foreach($dropout_action_list as $key=>$value){
+				if($bean->dropout_status==$key)
+					$action.="<option value='".$key."' selected>".$value."</option>";
+				else
+					$action.="<option value='".$key."'>".$value."</option>";			
+			}
+			$action.="</select>";
+			$bean->dropout_status=$action;
+		}	
+	}		
+}
