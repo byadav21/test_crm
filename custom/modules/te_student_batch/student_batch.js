a5a41e878f02df5fdbcfca8cf90690b9636bad61
Ravi Tiwari <ravibhushanblevel@gmail.com>
2017-03-02 21:08:02 -0500

    Update student_batch.js

--- custom/modules/te_student_batch/student_batch.js
+++ custom/modules/te_student_batch/student_batch.js
@@ -17,19 +17,3 @@ $(document).ready(function(){
 	})
 });
 
-function changeDropoutStatus(request_id,value){
-	var span_id="dropout_request_"+request_id;	
-	$("#"+span_id).html('<img id="previewimage" src="custom/themes/default/images/spin.gif" width="32" height="32"/>');	
-	jQuery.ajax({
-		type: "POST",
-		url: 'index.php?entryPoint=dropoutapprove',
-		data: {request_id: request_id,request_status: value},
-		success: function (result)
-		{
-			var result = JSON.parse(result);			
-			if(result.status=='Approved'){
-				 window.location.reload();
-			}
-		}
-	}); 	
-}
