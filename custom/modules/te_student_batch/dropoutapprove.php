091484f6077489047d29c73c53cce48a2d6f6eff
Ravi Tiwari <ravibhushanblevel@gmail.com>
2017-03-04 03:37:07 -0500

    Update dropoutapprove.php

--- custom/modules/te_student_batch/dropoutapprove.php
+++ custom/modules/te_student_batch/dropoutapprove.php
@@ -6,7 +6,7 @@ ini_set('memory_limit','1024M');
 require_once('include/entryPoint.php');
 global $db;
 
-$dropoutSql="UPDATE te_student_batch SET dropout_status='".$_REQUEST['request_status']."' WHERE id='".$_REQUEST['request_id']."'";
+$dropoutSql="UPDATE te_student_batch SET dropout_status='".$_REQUEST['request_status']."',refund_date='".$GLOBALS['timedate']->to_db_date($_REQUEST['refund_date'],false)."',refund_amount='".$_REQUEST['refund_amount']."',dropout_type='".$_REQUEST['dropout_type']."' WHERE id='".$_REQUEST['request_id']."'";
 $GLOBALS['db']->query($dropoutSql);
 
 $dropoutStatue['status']="Approved";
