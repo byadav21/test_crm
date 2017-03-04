41415b9a367afb2602f875809df65c9f58788aa9
Ravi Tiwari <ravibhushanblevel@gmail.com>
2017-03-04 03:36:05 -0500

    Update view.dropoutrequest.php

--- custom/modules/te_student_batch/views/view.dropoutrequest.php
+++ custom/modules/te_student_batch/views/view.dropoutrequest.php
@@ -24,20 +24,32 @@ class te_student_batchViewDropoutrequest extends SugarView {
 		$studentBatchSql="SELECT sb.*,s.name as student FROM te_student s INNER JOIN te_student_te_student_batch_1_c sbr ON s.id=sbr.te_student_te_student_batch_1te_student_ida INNER JOIN te_student_batch sb ON sbr.te_student_te_student_batch_1te_student_batch_idb=sb.id WHERE sb.dropout_status='Pending'  AND sb.deleted=0";
 		$studentBatchObj =$db->query($studentBatchSql);
 		$resultSet=array();
-		$dropout_action_list =$GLOBALS['app_list_strings']['dropuout_status_list'];
+		$dropout_status_list =$GLOBALS['app_list_strings']['dropuout_status_list'];
+		$dropout_type_list =$GLOBALS['app_list_strings']['student_batch_dropout_list'];
+		$rowcount=0;
 		while($row =$db->fetchByAssoc($studentBatchObj)){
+			$rowcount++;
 			$row['program']=$this->getProgram($row['te_pr_programs_id_c']);
 			$row['institute']=$this->getInstitute($row['te_in_institutes_id_c']);
 			
-			$action="<span id='dropout_request_".$row['id']."'></span><select name='dropout_status' id='".$row['id']."' onchange='return changeDropoutStatus(this.id,this.value);' style='width:113PX !IMPORTANT'><option value=''>--select--</option>";
-			foreach($dropout_action_list as $key=>$value){
+			$dropout_status="<span id='dropout_request_".$row['id']."'></span><select name='dropout_status' id='".$row['id']."' onchange='return changeDropoutStatus(this.id,this.value,".$rowcount.");' style='width:113PX !IMPORTANT'><option value=''></option>";
+			foreach($dropout_status_list as $key=>$value){
 				if($row['dropout_status']==$key)
-					$action.="<option value='".$key."' selected>".$value."</option>";
+					$dropout_status.="<option value='".$key."' selected>".$value."</option>";
 				else
-					$action.="<option value='".$key."'>".$value."</option>";			
+					$dropout_status.="<option value='".$key."'>".$value."</option>";			
 			}
-			$action.="</select>";
-			$row['action']=$action;
+			$dropout_status.="</select>";
+			$dropout_type="<select name='dropout_type' id='dropout_type_".$rowcount."'  style='width:90PX !IMPORTANT'>";
+			foreach($dropout_type_list as $key=>$value){
+				if($row['dropout_type']==$key)
+					$dropout_type.="<option value='".$key."' selected>".$value."</option>";
+				else
+					$dropout_type.="<option value='".$key."'>".$value."</option>";			
+			}
+			$dropout_type.="</select>";
+			$row['dropout_type']=$dropout_type;
+			$row['dropout_status']=$dropout_status;
 			$resultSet[]=$row;
 		}		
 		
