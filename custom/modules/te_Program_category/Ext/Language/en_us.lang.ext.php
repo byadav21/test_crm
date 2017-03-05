8dd583dffbe9ae779f0db34045d20106c521ff58
Avneet Singh <avneet.singh@engeniatech.com>
2017-03-05 19:32:14 +0530

    .gitignore is now working

--- custom/modules/te_Program_category/Ext/Language/en_us.lang.ext.php
+++
@@ -1,8 +0,0 @@
-<?php 
- //WARNING: The contents of this file are auto-generated
-
-
-//THIS FILE IS AUTO GENERATED, DO NOT MODIFY
-$mod_strings['LBL_TE_PROGRAM_CATEGORY_TE_PR_PROGRAMS_FROM_TE_PR_PROGRAMS_TITLE'] = 'Programs';
-
-?>
--- custom/modules/te_budgeted_campaign/Ext/Language/en_us.lang.ext.php
+++
@@ -1,9 +0,0 @@
-<?php 
- //WARNING: The contents of this file are auto-generated
-
-
-//THIS FILE IS AUTO GENERATED, DO NOT MODIFY
-$mod_strings['LBL_TE_UTM_TE_BUDGETED_CAMPAIGN_1_FROM_TE_UTM_TITLE'] = 'UTM';
-$mod_strings['LBL_CAMPAIGN_DATE'] = 'Date';
-
-?>
