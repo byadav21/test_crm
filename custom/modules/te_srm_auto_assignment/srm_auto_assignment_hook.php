9afa307fc30cf089c20cc9e2027f1362a112e493
Ravi Tiwari <ravibhushanblevel@gmail.com>
2017-03-12 00:40:18 -0500

    Update srm_auto_assignment_hook.php

--- custom/modules/te_srm_auto_assignment/srm_auto_assignment_hook.php
+++ custom/modules/te_srm_auto_assignment/srm_auto_assignment_hook.php
@@ -5,6 +5,14 @@ class SrmAutoAssignment{
 	function setName($bean, $event, $argument){		
 		if(!isset($_REQUEST['import_module']) && $_REQUEST['module']=="te_srm_auto_assignment"){
 			$bean->name=$_REQUEST['batch'];
+			if(empty($bean->fetched_row['id'])){
+				$duplicateSql = "SELECT id FROM te_srm_auto_assignment WHERE te_ba_batch_id_c='".$_REQUEST['te_ba_batch_id_c']."'";
+				$duplicateObj = $GLOBALS['db']->query($duplicateSql);
+				if($GLOBALS['db']->getRowCount($duplicateObj)>0){
+					SugarApplication::appendErrorMessage('You have been redirected here because the selected batch has already assigned to one SRM');
+					SugarApplication::redirect('index.php?module=te_srm_auto_assignment&action=EditView');
+				}					
+			}
 		}
 	}
 	
