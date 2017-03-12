5e82b8abf0c034fc73a70da9575db20a7532ad00
Ravi Tiwari <ravibhushanblevel@gmail.com>
2017-03-12 00:42:28 -0500

    Update view.feereport.php

--- custom/modules/AOR_Reports/views/view.feereport.php
+++ custom/modules/AOR_Reports/views/view.feereport.php
@@ -7,15 +7,35 @@ class AOR_ReportsViewFeereport extends SugarView {
 	public function __construct() {
 		parent::SugarView();
 	}
-	
+	function getBatch(){
+		global $db;	
+		$batchSql="SELECT id,name from te_ba_batch WHERE deleted=0 AND batch_status='enrollment_in_progress'";
+		$batchObj =$db->query($batchSql);
+		$batchOptions=array();
+		while($row =$db->fetchByAssoc($batchObj)){ 
+			$batchOptions[]=$row;
+		}
+		return $batchOptions;
+	}
 	public function display() {
 		global $db;
-       	
+       	#Get batch drop down option
+		$batchList=$this->getBatch();
+		# Query for batch drop down options
+		$where="";
+		$selected_batch="";
+		if(isset($_POST['button']) && $_POST['button']=="Search") {
+			if(!empty($_POST['batch'])){	
+				$selected_batch=$_POST['batch'];
+				$where.=" AND sb.te_ba_batch_id_c = '".$_POST['batch']."'";
+			}
+		}
+		
 		$installmentSql="SELECT count(spp.name)as total FROM te_student s INNER JOIN te_student_te_student_batch_1_c sbr ON s.id=sbr.te_student_te_student_batch_1te_student_ida INNER JOIN te_student_batch sb ON sbr.te_student_te_student_batch_1te_student_batch_idb=sb.id INNER JOIN te_student_batch_te_student_payment_plan_1_c sbpr ON sb.id=sbpr.te_student_batch_te_student_payment_plan_1te_student_batch_ida INNER JOIN te_student_payment_plan spp ON sbpr.te_student9d1ant_plan_idb=spp.id WHERE sb.status='Active' AND sb.deleted=0 AND spp.name<>'Initial Payment'GROUP BY s.name,sb.name order by total desc limit 0,1";
 		$installmentObj=$db->query($installmentSql);
 		$installment =$db->fetchByAssoc($installmentObj);
 				
-		$feeSql="SELECT s.name as student,s.email,s.mobile,sb.name as batch,spp.name as instalment,spp.due_date,spp.total_amount as due_amount,spp.paid_amount_inr as paid_amount FROM te_student s INNER JOIN te_student_te_student_batch_1_c sbr ON s.id=sbr.te_student_te_student_batch_1te_student_ida INNER JOIN te_student_batch sb ON sbr.te_student_te_student_batch_1te_student_batch_idb=sb.id INNER JOIN te_student_batch_te_student_payment_plan_1_c sbpr ON sb.id=sbpr.te_student_batch_te_student_payment_plan_1te_student_batch_ida INNER JOIN te_student_payment_plan spp ON sbpr.te_student9d1ant_plan_idb=spp.id WHERE sb.status='Active' AND sb.deleted=0 AND spp.name<>'Initial Payment' ORDER BY s.name,sb.name,spp.name";
+		$feeSql="SELECT s.name as student,s.email,s.mobile,sb.name as batch,spp.name as instalment,spp.due_date,spp.total_amount as due_amount,spp.paid_amount_inr as paid_amount FROM te_student s INNER JOIN te_student_te_student_batch_1_c sbr ON s.id=sbr.te_student_te_student_batch_1te_student_ida INNER JOIN te_student_batch sb ON sbr.te_student_te_student_batch_1te_student_batch_idb=sb.id INNER JOIN te_student_batch_te_student_payment_plan_1_c sbpr ON sb.id=sbpr.te_student_batch_te_student_payment_plan_1te_student_batch_ida INNER JOIN te_student_payment_plan spp ON sbpr.te_student9d1ant_plan_idb=spp.id WHERE sb.status='Active' AND sb.deleted=0 ".$where." ORDER BY s.name,sb.name,spp.name";
 		
 		$feeObj =$db->query($feeSql);
 		$councelorList=array();
@@ -34,23 +54,24 @@ class AOR_ReportsViewFeereport extends SugarView {
 				$feeList[$index]['Email']=$row['email'];
 				$feeList[$index]['Batch']=$row['batch'];
 				$feeList[$index]['Phone']=$row['mobile'];
-				$feeList[$index]['Instalment 1 Due Date']=$row['due_date'];
-				$feeList[$index]['Instalment 1 Due Amount']=$row['due_amount'];
-				$feeList[$index]['Instalment 1 Paid Amount']=$row['paid_amount'];
+				$feeList[$index]['Initial Due Date']=$row['due_date'];
+				$feeList[$index]['Initial Due Amount']=$row['due_amount'];
+				$feeList[$index]['Initial Paid Amount']=$row['paid_amount'];
 				$index++;
-				$next=2;
+				$next=1;
 			}		
 		}	
-		$reportHeader=array("Student","Email","Phone","Batch");
+		$reportHeader=array("Student","Email","Phone","Batch","Initial Due Date","Initial Due Amount","Initial Paid Amount");
 		for($x=1;$x<=$installment['total'];$x++){
 			$reportHeader[]='Instalment '.$x.' Due Date';
 			$reportHeader[]='Instalment '.$x.' Due Amount';
 			$reportHeader[]='Instalment '.$x.' Paid Amount';
 		}
-	
 		$sugarSmarty = new Sugar_Smarty();
 		$sugarSmarty->assign("reportHeader",$reportHeader);
 		$sugarSmarty->assign("feeList",$feeList);
+		$sugarSmarty->assign("batchList",$batchList);
+		$sugarSmarty->assign("selected_batch",$selected_batch);
 		$sugarSmarty->display('custom/modules/AOR_Reports/tpls/feereport.tpl');
 	}
 }
