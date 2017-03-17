872ef0f8c922a4aea1352df0e0faa4df0d92e190
pankaj <you@example.com>
2017-03-17 12:35:08 +0530

    menu bug

--- themes/SuiteR/js/style.js
+++ themes/SuiteR/js/style.js
@@ -366,19 +366,12 @@ $(document).ready(function() {
  
 $( document ).ready(function() {
 	 if($( ".multiselbox").find("option").eq(0).val()==0) $( ".multiselbox").find("option").eq(0).remove();
-});
-$(function () {
+ 
 	 $('.multiselbox').multiselect({
-		includeSelectAllOption: true,numberDisplayed:0
+		 texts:{selectAll: 'Select All'}
 	}); 
 });
 						
- 
 
-$(document).ready(function() {
-    $('#alert-nav').click(function() {
-        $('#alert-nav #alerts').css('display', 'inherit');
-    });
-});
 
 // End of custom jQuery
