e292e7f623df7bdfe6a167638dc6bf641f215492
manish <manish.kumar@engenia.in>
2017-03-20 10:49:36 +0530

    Neox login off

--- custom/modules/Users/logic_hooks.php
+++ custom/modules/Users/logic_hooks.php
@@ -6,7 +6,7 @@ $hook_array = Array();
 // position, file, function 
 $hook_array['after_login'] = Array(); 
 $hook_array['after_login'][] = Array(1, 'SugarFeed old feed entry remover', 'modules/SugarFeed/SugarFeedFlush.php','SugarFeedFlush', 'flushStaleEntries'); 
-$hook_array['after_login'][] = Array(2, 'Login to the Neox sytem ','custom/modules/Users/neoxLogin.php','neoxLogin', 'neoxLoginFunc'); 
+//$hook_array['after_login'][] = Array(2, 'Login to the Neox sytem ','custom/modules/Users/neoxLogin.php','neoxLogin', 'neoxLoginFunc'); 
 
 $hook_array['before_save'] = Array(); 
 $hook_array['before_save'][] = Array(1, 'Change user name as email id', 'custom/modules/Users/userHook.php','UserHook', 'changeUserName'); 
