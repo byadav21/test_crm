6d100aa41b1b44ed5f6149c6a4d4a35da9b362e6
pankaj <you@example.com>
2017-03-20 18:25:29 +0530

    sso

---
+++ custom/Extension/application/Ext/EntryPointRegistry/apiLogin.php
@@ -0,0 +1,5 @@
+<?php
+$entry_point_registry['loginCRM'] = array('file' => 'custom/modules/te_Api/login.php','auth' => false);
+$entry_point_registry['logoutCRM'] = array('file' => 'custom/modules/te_Api/logout.php','auth' => false);
+
+?>
