02b1ba2b4dca2943aac86b9fde9f735fbb3b8c78
pankaj <you@example.com>
2017-03-21 12:10:42 +0530

    acl transfer batch

--- custom/modules/te_transfer_batch/transfer_batch_hook.php
+++ custom/modules/te_transfer_batch/transfer_batch_hook.php
@@ -1,18 +1,30 @@
 <?php
 if (!defined('sugarEntry') || !sugarEntry) die('Not A Valid Entry Point');
+ 
 class BatchTransferRequest{	
 	function approveBatchTransferRequest($bean, $event, $argument){				
 		if($bean->status=="Pending"){
-			$batch_action_list =$GLOBALS['app_list_strings']['dropuout_status_list'];
-			$action="<span id='batch_transfer_request_".$bean->id."'></span><select name='status' id='".$bean->id."' onchange='return changeTransferStatus(this.id,this.value);'><option value='' style='width:30px;'>--select--</option>";
-			foreach($batch_action_list as $key=>$value){
-				if($bean->status==$key)
-					$action.="<option value='".$key."' selected>".$value."</option>";
-				else
-					$action.="<option value='".$key."'>".$value."</option>";			
-			}
-			$action.="</select>";
-			$bean->status=$action;
+			 global $current_user;
+			// print_r($_SESSION['ACL'][$current_user->id]['te_transfer_batch']); 
+			 
+			 
+			if(ACLController::checkAccess('te_transfer_batch','edit')){
+				
+				$batch_action_list =$GLOBALS['app_list_strings']['dropuout_status_list'];
+				$action="<span id='batch_transfer_request_".$bean->id."'></span><select name='status' id='".$bean->id."' onchange='return changeTransferStatus(this.id,this.value);'><option value='' style='width:30px;'>--select--</option>";
+				foreach($batch_action_list as $key=>$value){
+					if($bean->status==$key)
+						$action.="<option value='".$key."' selected>".$value."</option>";
+					else
+						$action.="<option value='".$key."'>".$value."</option>";			
+				}
+				$action.="</select>";
+				$bean->status=$action;
+				
+			} 
+			
+			
+			
 		}
 		global $db;
 		$sql="select name from te_ba_batch where id='".$bean->te_student_batch_id_c."' and deleted=0";
