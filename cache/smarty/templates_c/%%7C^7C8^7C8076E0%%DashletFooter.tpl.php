c94f7c9c97f168c25953887b68b5da3bbf0d3584
pankaj <you@example.com>
2017-03-22 15:31:39 +0530

    cachedel

--- cache/smarty/templates_c/%%7C^7C8^7C8076E0%%DashletFooter.tpl.php
+++
@@ -1,3 +0,0 @@
-<?php /* Smarty version 2.6.29, created on 2017-03-22 15:24:52
-         compiled from include/Dashlets/DashletFooter.tpl */ ?>
-</div><div class="mr"></div></div><div class="ft"><div class="bl"></div><div class="ft-center"></div><div class="br"></div></div>
