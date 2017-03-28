826f1bd656b904ddd8f365e5220abd599d8cb0a9
pankaj <you@example.com>
2017-03-28 12:37:25 +0530

    transfer batch

---
+++ custom/Extension/modules/te_transfer_batch/Ext/Vardefs/file.php
@@ -0,0 +1,5 @@
+<?php
+
+$dictionary['te_transfer_batch']['fields']['old_batch_records']['name']='old_batch_records';
+$dictionary['te_transfer_batch']['fields']['old_batch_records']['vname']='old_batch_records';
+$dictionary['te_transfer_batch']['fields']['old_batch_records']['type']='varchar';
