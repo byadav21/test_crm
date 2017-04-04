c115702907e9f41e291ca1b96a50031d7baa5c33
pankaj <you@example.com>
2017-04-04 11:02:11 +0530

    inbound api

--- custom/modules/Leads/leadCRM.php
+++ custom/modules/Leads/leadCRM.php
@@ -6,6 +6,8 @@ require_once('custom/modules/te_student/te_student_override.php');
 global $current_user,$db;
 $customerId= $_REQUEST['customerId'];
 $userID= $_REQUEST['userId'];
+$phone= $_REQUEST['phone'];
+$callType= $_REQUEST['callType'];
 
 //if($userID==$)
 
@@ -14,13 +16,33 @@ $getUserIDs= "select id from users where user_name='$userID'";
 $getUserID=$db->query($getUserIDs);
 if($db->getRowCount($getUserID) > 0){
 	$userid=$db->fetchByAssoc($getUserID);
-		
-	$lead="select id from  leads where  dristi_customer_id='$customerId'";
+	if($callType=='outbound.auto.dial'){	
+		$lead="select id from  leads where  dristi_customer_id='$customerId'";
+	}else if($callType=='inbound.call.dial'){	
+		$lead="select id,assigned_user_id from  leads where ( phone_home='$phone' or  phone_mobile='$phone' or  phone_work='$phone' or  phone_other='$phone' ) ";
+	}	
 	$res=$db->query($lead);
 	if($db->getRowCount($res) > 0){	
-		$records=$db->fetchByAssoc($res);		
-		$db->query("update leads set dristi_request='".  json_encode($_REQUEST) ."',assigned_user_id='". $userid['id'] ."' where id='". $records['id'] ."'");		
-		header('Location: index.php?module=Leads&action=DetailView&record='. $records['id']);
+		$records=$db->fetchByAssoc($res);	
+		if($callType=='outbound.auto.dial'){		
+			$db->query("update leads set dristi_request='".  json_encode($_REQUEST) ."',assigned_user_id='". $userid['id'] ."' where id='". $records['id'] ."'");		
+			header('Location: index.php?module=Leads&action=DetailView&record='. $records['id']);
+		}else if($callType=='inbound.call.dial'){
+			
+			if(empty($records['assigned_user_id'])){
+				
+				$db->query("update leads set dristi_request='".  json_encode($_REQUEST) ."',assigned_user_id='". $userid['id'] ."' where id='". $records['id'] ."'");		
+				header('Location: index.php?module=Leads&action=DetailView&record='. $records['id']);
+				
+			}else if($records['assigned_user_id']!=$userid['id']){
+				
+				header('Location: index.php?module=te_student_batch&action=search_leads&search_leads=1&mobile_number='. $phone);
+				 
+			}else{
+				header('Location: index.php?module=Leads&action=DetailView&record='. $records['id']);
+			}
+			
+		}
 	}	
 }else{
 	
