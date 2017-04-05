2c73647933b04ee2140ee9492b8adaba17f1fbb8
Ravi Tiwari <ravibhushanblevel@gmail.com>
2017-04-05 09:14:15 -0400

    Update UpdatePayment.php

--- custom/modules/te_payment_details/UpdatePayment.php
+++ custom/modules/te_payment_details/UpdatePayment.php
@@ -3,16 +3,10 @@ if (!defined('sugarEntry') || !sugarEntry) die('Not A Valid Entry Point');
 class UpdatePaymentName{
 	
 	function UpdatePaymentFunc($bean, $event, $argument){
-		$lead_id="";		
+		$lead_id="";
 		if(!empty($bean->name)){
 			$sa = "UPDATE te_payment_details SET name='".$bean->reference_number."' WHERE id='".$bean->id."'";
-				//~ echo $s;
-				$GLOBALS['db']->query($sa);
-			//~ $bean->name = $bean->reference_number;
-		}
-		# if payment details is being updated. Update the same payment in student payment module
-		if(isset($_REQUEST['record'])&&$_REQUEST['record']!=""){
-			$GLOBALS['db']->query("UPDATE te_student_payment SET amount='".$bean->amount."' WHERE lead_payment_details_id='".$_REQUEST['record']."'");
+			$GLOBALS['db']->query($sa);
 		}
 		
 		$leadSql = "SELECT leads_te_payment_details_1leads_ida as lid FROM leads_te_payment_details_1_c WHERE leads_te_payment_details_1te_payment_details_idb = '".$bean->id."' AND deleted = 0";
@@ -22,21 +16,22 @@ class UpdatePaymentName{
 			$leadRow = $GLOBALS['db']->fetchByAssoc($relLead);
 			$lead_id = $leadRow['lid'];
 			$sqlRel = "SELECT p.id FROM te_payment_details p INNER JOIN leads_te_payment_details_1_c lp ON p.id=lp.leads_te_payment_details_1te_payment_details_idb WHERE lp.leads_te_payment_details_1leads_ida='".$leadRow['lid']."' AND p.payment_realized= 0 ";
-			//~ echo $sqlRel."<br>";
 			$rel= $GLOBALS['db']->query($sqlRel);
 			if($GLOBALS['db']->getRowCount($rel) > 0){
 				$s = "UPDATE leads SET payment_realized_check=0 WHERE id='".$leadRow['lid']."'";
-				//~ echo $s;
-				$GLOBALS['db']->query($s);
+			$GLOBALS['db']->query($s);
 			}
 			else{
 				$s = "UPDATE leads SET payment_realized_check=1 WHERE id='".$leadRow['lid']."'";
-				//~ echo $s;
 				$GLOBALS['db']->query($s);
 			}
 		}
-		if($lead_id!=""){
+		
+		# if payment details is being updated. Update the same payment in student payment module
+		if(isset($_REQUEST['record'])&&$_REQUEST['record']!=""){
+			$GLOBALS['db']->query("UPDATE te_student_payment SET amount='".$bean->amount."' WHERE lead_payment_details_id='".$_REQUEST['record']."'");
 			
+			#update student payment plan			
 			$paymentSql = "SELECT SUM(p.amount) as amount FROM te_payment_details p INNER JOIN leads_te_payment_details_1_c lp ON p.id=lp.leads_te_payment_details_1te_payment_details_idb WHERE lp.leads_te_payment_details_1leads_ida='".$lead_id."' AND p.payment_realized= 1";
 			$paymentObj= $GLOBALS['db']->query($paymentSql);
 			$paymentRes=$GLOBALS['db']->fetchByAssoc($paymentObj);
@@ -57,6 +52,7 @@ class UpdatePaymentName{
 		
 				$this->updateStudentPaymentPlan($paymentDetails);
 			}
+		
 		}
 	}
 	function removePaymentPlan($student_id,$batch_id,$student_country){
