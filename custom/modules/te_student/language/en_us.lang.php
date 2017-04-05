92ca5914c7408a011a4bd20f134cff456bf5d2dd
manish <manish.kumar@engenia.in>
2017-04-05 12:42:16 +0530

    add New Status Field from student subpannel student batch

--- custom/modules/te_student/language/en_us.lang.php
+++ custom/modules/te_student/language/en_us.lang.php
@@ -1,5 +1,6 @@
 <?php
-// created: 2017-03-17 16:52:34
+// created: 2017-04-05 00:10:34
 $mod_strings = array (
   'LBL_TE_STUDENT_TE_STUDENT_DISPOSITION_1_FROM_TE_STUDENT_DISPOSITION_TITLE' => 'Student Notes',
+  'LBL_TE_STUDENT_TE_STUDENT_BATCH_1_FROM_TE_STUDENT_BATCH_TITLE' => 'Student Batch',
 );
