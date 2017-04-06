5c3fa91063640706dea174bcc5bda8e9a31dede6
anup.kumar@engenia.in <anup.kumar@engenia.in>
2017-04-06 19:27:39 +0530

    Student Batch detail payment plan show total

--- custom/modules/te_student_batch/metadata/detailviewdefs.php
+++ custom/modules/te_student_batch/metadata/detailviewdefs.php
@@ -1,14 +1,14 @@
 <?php
 $module_name = 'te_student_batch';
-$viewdefs [$module_name] = 
+$viewdefs [$module_name] =
 array (
-  'DetailView' => 
+  'DetailView' =>
   array (
-    'templateMeta' => 
+    'templateMeta' =>
     array (
-      'form' => 
+      'form' =>
       array (
-        'buttons' => 
+        'buttons' =>
         array (
           0 => 'EDIT',
           1 => 'DUPLICATE',
@@ -17,215 +17,219 @@ array (
         ),
       ),
       'maxColumns' => '2',
-      'widths' => 
+      'widths' =>
       array (
-        0 => 
+        0 =>
         array (
           'label' => '10',
           'field' => '30',
         ),
-        1 => 
+        1 =>
         array (
           'label' => '10',
           'field' => '30',
         ),
       ),
-      'includes' => 
+      'includes' =>
       array (
-        0 => 
+        0 =>
         array (
           'file' => 'custom/modules/te_student_batch/student_batch.js',
         ),
       ),
       'useTabs' => false,
-      'tabDefs' => 
+      'tabDefs' =>
       array (
-        'DEFAULT' => 
+        'DEFAULT' =>
         array (
           'newTab' => false,
           'panelDefault' => 'expanded',
         ),
-        'LBL_EDITVIEW_PANEL1' => 
+        'LBL_EDITVIEW_PANEL1' =>
         array (
           'newTab' => false,
           'panelDefault' => 'expanded',
         ),
       ),
     ),
-    'panels' => 
+    'panels' =>
     array (
-      'default' => 
+      'default' =>
       array (
-        0 => 
+        0 =>
         array (
-          0 => 
+          0 =>
           array (
             'name' => 'batch',
             'studio' => 'visible',
             'label' => 'LBL_BATCH',
           ),
-          1 => 
+          1 =>
           array (
             'name' => 'batch_code',
             'label' => 'LBL_BATCH_CODE',
           ),
         ),
-        1 => 
+        1 =>
         array (
-          0 => 
+          0 =>
           array (
             'name' => 'te_student_te_student_batch_1_name',
           ),
-          1 => 
+          1 =>
           array (
             'name' => 'institute',
             'studio' => 'visible',
             'label' => 'LBL_INSTITUTE',
           ),
         ),
-        2 => 
+        2 =>
         array (
-          0 => 
+          0 =>
           array (
             'name' => 'program',
             'studio' => 'visible',
             'label' => 'LBL_PROGRAM',
           ),
-         /* 1 => 
+         /* 1 =>
           array (
             'name' => 'source',
             'studio' => 'visible',
             'label' => 'LBL_SOURCE',
           ),*/
         ),
-        3 => 
+        3 =>
         array (
-          0 => 
+          0 =>
           array (
             'name' => 'fee_inr',
             'label' => 'LBL_FEE_INR',
           ),
-          1 => 
+          1 =>
           array (
             'name' => 'fee_usd',
             'label' => 'LBL_FEE_USD',
           ),
         ),
-        4 => 
+        4 =>
         array (
-          0 => 
+          0 =>
           array (
             'name' => 'status',
             'studio' => 'visible',
             'label' => 'LBL_STATUS',
           ),
-          1 => 
+          1 =>
           array (
             'name' => 'result',
             'studio' => 'visible',
             'label' => 'LBL_RESULT',
           ),
         ),
-        5 => 
+        5 =>
         array (
-          0 => 
+          0 =>
           array (
             'name' => 'eligible_for_certificate',
             'studio' => 'visible',
             'label' => 'LBL_ELIGIBLE_FOR_CERTIFICATE',
           ),
-          1 => 
+          1 =>
           array (
             'name' => 'assessment_mode',
             'studio' => 'visible',
             'label' => 'LBL_ASSESSMENT_MODE',
           ),
         ),
-        6 => 
+        6 =>
         array (
-          0 => 
+          0 =>
           array (
             'name' => 'actual_attendance',
             'label' => 'LBL_ACTUAL_ATTENDANCE',
           ),
-          1 => 
+          1 =>
           array (
             'name' => 'feedback_given',
             'studio' => 'visible',
             'label' => 'LBL_FEEDBACH_GIVEN',
           ),
         ),
-        7 => 
+        7 =>
         array (
-          0 => 
+          0 =>
           array (
             'name' => 'Assessment_center_lcocation_preference',
             'label' => 'LBL_ASSESSMENT_CENTER_LOCATION_PREFERENCE',
           ),
-          1 => 
+          1 =>
           array (
             'name' => 'total_session_required',
             'label' => 'LBL_TOTAL_SESSION_REQUIRED',
           ),
         ),
-        8 => 
+        8 =>
         array (
-          0 => 
+          0 =>
           array (
             'name' => 'channel',
             'label' => 'LBL_CHANNEL',
           ),
-          1 => 
+          1 =>
           array (
             'name' => 'completion_certificate_address',
             'comment' => 'Full text of the note',
             'label' => 'LBL_COMPLETION_CERTIFICATE_ADDRESS',
           ),
         ),
-        9 => 
+        9 =>
         array (
           0 => 'assigned_user_name',
           1 => 'description',
         ),
-      ),
-      'lbl_editview_panel1' => 
-      array (
-        0 => 
+        10 =>
         array (
-          0 => 
+          'name' => 'total_payment',
+        ),
+      ),
+      'lbl_editview_panel1' =>
+      array (
+        0 =>
+        array (
+          0 =>
           array (
             'name' => 'dropout_type',
             'studio' => 'visible',
             'label' => 'LBL_DROPOUT_TYPE',
           ),
-          1 => 
+          1 =>
           array (
             'name' => 'qualify_for_refund',
             'studio' => 'visible',
             'label' => 'LBL_QUALIFY_FOR_REFUND',
           ),
         ),
-        1 => 
+        1 =>
         array (
-          0 => 
+          0 =>
           array (
             'name' => 'refund_request_date',
             'label' => 'LBL_REFUND_REQUEST_DATE',
           ),
-          1 => 
+          1 =>
           array (
             'name' => 'refund_amount',
             'label' => 'LBL_REFUND_AMOUNT',
           ),
         ),
-        2 => 
+        2 =>
         array (
-          0 => 
+          0 =>
           array (
             'name' => 'refund_date',
             'label' => 'LBL_REFUND_DATE',
           ),
-          1 => 
+          1 =>
           array (
             'name' => 'dropout_status',
             'studio' => 'visible',
--- custom/modules/te_student_batch/student_batch.js
+++ custom/modules/te_student_batch/student_batch.js
@@ -16,4 +16,7 @@ $(document).ready(function(){
 		}
 	})*/
 	$(".actionmenulinks a").eq(4).attr('target','_blank');
+	$("#total_payment").closest('tr').hide();
+	var total_payment=$("#total_payment").text();
+	$("#list_subpanel_te_student_batch_te_student_payment_plan_1>table>tbody").last('tr').after("<tr><th colspan='8' style='border: 1px solid #ddd;font-weight: bold;font-size: 1.5em;text-align: right;'>Total Payment: "+total_payment+"</th></tr>");
 });
