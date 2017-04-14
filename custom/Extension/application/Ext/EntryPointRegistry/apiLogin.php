27ae9348059817fc3aa1d3fdd81ef4a9dde5a121
pankaj <you@example.com>
2017-04-14 12:50:37 +0530

    dispose Mannually

--- custom/Extension/application/Ext/EntryPointRegistry/apiLogin.php
+++ custom/Extension/application/Ext/EntryPointRegistry/apiLogin.php
@@ -4,5 +4,6 @@ $entry_point_registry['logoutCRM'] = array('file' => 'custom/modules/te_Api/logo
 $entry_point_registry['ivrCRM'] = array('file' => 'custom/modules/te_Api/ivr.php','auth' => false);
 $entry_point_registry['dashboard'] = array('file' => 'custom/modules/te_Api/dashboard.php','auth' => false);
 $entry_point_registry['dispose'] = array('file' => 'custom/modules/te_Api/dispose.php','auth' => false);
+$entry_point_registry['disposeManually'] = array('file' => 'custom/modules/te_Api/disposeManually.php','auth' => false);
 
 ?>
