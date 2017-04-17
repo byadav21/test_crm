c30216a4d7814b47b0b6b151fbab96051901bf9b
manish <manish.kumar@engenia.in>
2017-04-17 15:41:41 +0530

    add code for mail trigger

--- custom/modules/te_student_batch/dropoutapprove.php
+++ custom/modules/te_student_batch/dropoutapprove.php
@@ -4,6 +4,7 @@ if (!defined('sugarEntry') || !sugarEntry)
 
 ini_set('memory_limit','1024M');
 require_once('include/entryPoint.php');
+require_once('custom/include/Email/sendmail.php');
 global $db;
 
 $dropoutSql="UPDATE te_student_batch SET is_new_approved=1,dropout_status='".$_REQUEST['request_status']."',refund_date='".$GLOBALS['timedate']->to_db_date($_REQUEST['refund_date'],false)."',refund_amount='".$_REQUEST['refund_amount']."',dropout_type='".$_REQUEST['dropout_type']."', approved_by='".$_REQUEST['current_user_id']."' WHERE id='".$_REQUEST['request_id']."'";
@@ -27,10 +28,55 @@ $disposition->save();
 
 $sqlL = "UPDATE leads SET status='Dropout',status_description='Dropout' WHERE id ='".$lead_id."'";
 $GLOBALS['db']->query($sqlL);
-//------------------------------------------------------
-
-
+	
 $dropoutStatue['status']="Approved";
+
+#mail for dropot apporoved /reject
+
+if(isset($_REQUEST['request_status']) &&$_REQUEST['request_status']=="Rejected"){
+	global $db;
+			
+			$studentSql="SELECT email,name FROM `te_student` WHERE lead_id_c = '".$lead_id."'";
+			$studentObj = $db->query($studentSql);
+			$student = $db->fetchByAssoc($studentObj);
+			$studentemail=$student['email'];
+			
+			$template="<p>Hello ".$student['name']."</p>
+						<p>Your Dropout Request Rejected </p>
+						<p>Please have a look and take action accordingly</p>
+						<p></p><p>Thanks & Regards</p>
+						<p>SRM Team</p>";
+					
+			$mail = new NetCoreEmail();
+			$mail->sendEmail($studentemail,"Request",$template);
+	
+		
+		}
+#mail for dropot apporoved /reject -2 ++
+
+if(isset($_REQUEST['request_status']) && $_REQUEST['request_status']=="Approved"){
+	
+	global $db;
+			
+			$studentSql="SELECT email,name FROM `te_student` WHERE lead_id_c = '".$lead_id."'";
+			$studentObj = $db->query($studentSql);
+			$student = $db->fetchByAssoc($studentObj);
+			$studentemail=$student['email'];
+			
+			$template="<p>Hello ".$student['name']."</p>
+					    <p> Refaund Amount - ".$_REQUEST['refund_amount']."</p>
+					     <p> Refaund date - ".$_REQUEST['refund_date']."</p>
+						<p>Your Dropout hasbeen Approved</p>
+						<p>Please Waite For Appovel </p>
+						<p>Please have a look and take action accordingly</p>
+						<p></p><p>Thanks & Regards</p>
+						<p>SRM Team</p>";
+					
+			$mail = new NetCoreEmail();
+			$mail->sendEmail($studentemail,"Dropout Request",$template);
+		
+		}	
+
 echo json_encode($dropoutStatue);
 return false;
 
