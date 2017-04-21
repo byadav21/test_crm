a8fb4baefc1ab88d08e2cfd8ec7f964e893cb5f7
pawan <pawan.telentedge.in>
2017-04-21 16:59:40 +0530

    actual campaign summery v.1.1

---
+++ custom/Extension/modules/te_actual_campaign/Ext/Menus/menu.ext.php
@@ -0,0 +1,8 @@
+<?php
+if(!defined('sugarEntry') || !sugarEntry) die('Not A Valid Entry Point'); 
+global $mod_strings, $app_strings, $sugar_config;
+// This will add the new option @Pawan
+$module_menu[]=Array("index.php?module=te_actual_campaign&action=actual_campaign_summary", "Actual Campaign Summary", "");
+
+
+?>
