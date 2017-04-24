ed506e85eeda5d546779774ca0a566295e8dd460
manish <manish.kumar@engenia.in>
2017-04-24 12:23:36 +0530

    batch add/update web services

---
+++ custom/Extension/modules/te_ba_Batch/Ext/Vardefs/sugarfield_is_sent_web.php
@@ -0,0 +1,19 @@
+<?php
+$dictionary['te_ba_Batch']['fields']['is_sent_web'] =array (
+		'name' => 'is_sent_web',
+		'vname' => 'Is sent web',
+		'type' => 'int',
+		'required' => false,
+		'comments' => 'web api leads update or insert for batch',
+		'help' => '',
+		'default'=> 0,
+		'importable' => 'false',
+		'duplicate_merge' => 'disabled',
+		'duplicate_merge_dom_value' => '0',
+		'audited' => false,
+		'reportable' => false,
+		'len' => '50',
+		'size' => '50',
+	);
+
+?>
