1b6205b76e30fb246856cb4fea4e1a1339d8a9ca
manish <manish.kumar@engenia.in>
2017-04-25 12:37:48 +0530

    new field

---
+++ custom/Extension/modules/te_pr_Programs/Ext/Vardefs/sugarfield_batch_webid_new.php
@@ -0,0 +1,18 @@
+<?php
+$dictionary['te_pr_Programs']['fields']['web_id'] =array (
+		'name' => 'web_id',
+		'vname' => 'Web id',
+		'type' => 'varchar',
+		'required' => false,
+		'comments' => 'web id',
+		'help' => '',
+		'importable' => 'false',
+		'duplicate_merge' => 'disabled',
+		'duplicate_merge_dom_value' => '0',
+		'audited' => false,
+		'reportable' => false,
+		'len' => '50',
+		'size' => '50',
+	);
+
+?>
