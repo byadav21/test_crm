9aa775f9d8e8983438de99523290b7a8caebdfc6
pawan <pawan.telentedge.in>
2017-05-10 15:33:26 +0530

    nothing changed

--- custom/Extension/modules/te_ba_Batch/Ext/Vardefs/sugarfield_name.php
+++ custom/Extension/modules/te_ba_Batch/Ext/Vardefs/sugarfield_name.php
@@ -6,16 +6,4 @@ $dictionary['te_ba_Batch']['fields']['name']['duplicate_merge_dom_value']='0';
 $dictionary['te_ba_Batch']['fields']['name']['merge_filter']='disabled';
 $dictionary['te_ba_Batch']['fields']['name']['unified_search']=false;
 
-$dictionary['te_ba_Batch']['fields']['name']['importable']=true;
-$dictionary['te_ba_Batch']['fields']['batch_code']['importable']=true;
-$dictionary['te_ba_Batch']['fields']['date_entered']['importable']=true;
-$dictionary['te_ba_Batch']['fields']['date_modified']['importable']=true;
-$dictionary['te_ba_Batch']['fields']['batch_start_date']['importable']=true;
-$dictionary['te_ba_Batch']['fields']['duration']['importable']=true;
-$dictionary['te_ba_Batch']['fields']['registration_closing_date']['importable']=true;
-$dictionary['te_ba_Batch']['fields']['batch_size']['importable']=true;
-$dictionary['te_ba_Batch']['fields']['batch_start_date1']['importable']=true;
-
-
-
  ?>
