406f9b7f3d86458fe5f8ed92d7dc007a2d73567a
Pankaj Jha <pankaj.jha@talentedge.in>
2017-05-22 07:54:13 -0400

    Revert "Merge branch 'pawan' into 'dev_new'"
    This reverts merge request !167

--- custom/Extension/application/Ext/Include/Expensepo_Vendor.php
+++
@@ -1,7 +0,0 @@
-<?php 
- //WARNING: The contents of this file are auto-generated
-$beanList['te_Expensepo_Vendor'] = 'te_Expensepo_Vendor';
-$beanFiles['te_Expensepo_Vendor'] = 'modules/te_Expensepo_Vendor/te_Expensepo_Vendor.php';
-$moduleList[] = 'te_Expensepo_Vendor';
-
-?>
