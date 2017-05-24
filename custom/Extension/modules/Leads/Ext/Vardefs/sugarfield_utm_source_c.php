2ec61b501799852edfd8325178fe615da260ee3b
pankaj <you@example.com>
2017-05-24 12:51:37 +0530

    utm upload

---
+++ custom/Extension/modules/Leads/Ext/Vardefs/sugarfield_utm_source_c.php
@@ -0,0 +1,6 @@
+<?php
+ // created: 2017-05-22 17:42:12
+$dictionary['Lead']['fields']['utm_source_c']['inline_edit']='1';
+$dictionary['Lead']['fields']['utm_source_c']['labelValue']='utm source';
+
+ ?>
---
+++ custom/Extension/modules/Leads/Ext/Vardefs/sugarfield_utm_term_c.php
@@ -0,0 +1,6 @@
+<?php
+ // created: 2017-05-22 17:42:40
+$dictionary['Lead']['fields']['utm_term_c']['inline_edit']='1';
+$dictionary['Lead']['fields']['utm_term_c']['labelValue']='utm term';
+
+ ?>
