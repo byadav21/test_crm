44d6cfa3f38d0b150c9dbfa19ca6ce2d062a983a
Pankaj <pankaj.jha@talentedge.in>
2017-06-15 15:57:52 +0000

    uploads

--- custom/Extension/modules/Leads/Ext/Vardefs/sugarfield_vendor.php
+++ custom/Extension/modules/Leads/Ext/Vardefs/sugarfield_vendor.php
@@ -15,4 +15,57 @@ $dictionary['Lead']['fields']['vendor'] =array (
 	'importable' => 'true', 
 	'duplicate_merge' => false, 
 );
+
+
+$dictionary['Lead']['fields']['utm_term_c'] =array (
+        'name' => 'utm_term_c',
+        'label' => 'UTM Terms (Batch)',
+        'type' => 'varchar',
+        'module' => 'Leads',
+        'help' => '',
+        'comment' => '',
+        'default_value' => '',
+        'len' => '255',
+		'size' => '20',
+        'required' => false,
+        'reportable' => true,
+        'audited' => false,
+        'importable' => 'true',
+        'duplicate_merge' => false,
+);
+$dictionary['Lead']['fields']['utm_source_c'] =array (
+        'name' => 'utm_source_c',
+        'label' => 'UTM Source (vendor)',
+        'type' => 'varchar',
+        'module' => 'Leads',
+        'help' => '',
+        'comment' => '',
+        'default_value' => '',
+        'len' => '255',
+		'size' => '20',
+        'required' => false,
+        'reportable' => true,
+        'audited' => false,
+        'importable' => 'true',
+        'duplicate_merge' => false,
+);
+$dictionary['Lead']['fields']['utm_contract_c'] =array (
+        'name' => 'utm_contract_c',
+        'label' => 'UTM Mediums',
+        'type' => 'varchar',
+        'module' => 'Leads',
+        'help' => '',
+        'comment' => '',
+        'default_value' => '',
+        'len' => '255',
+		'size' => '20',
+        'required' => false,
+        'reportable' => true,
+        'audited' => false,
+        'importable' => 'true',
+        'duplicate_merge' => false,
+);
+
+
+
  ?>
