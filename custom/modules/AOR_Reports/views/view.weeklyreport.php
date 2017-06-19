da1d9ea2d0bcdcbe11a7b1e01026775e51c61d07
mranupkmr <mr.anupkmr@gmail.com>
2017-06-19 20:52:11 +0530

    weekly report fixed

--- custom/modules/AOR_Reports/views/view.weeklyreport.php
+++ custom/modules/AOR_Reports/views/view.weeklyreport.php
@@ -1,9 +1,9 @@
 <?php
 if (!defined('sugarEntry') || !sugarEntry)
 	die('Not A Valid Entry Point');
-require_once('custom/include/Email/sendmail.php'); 
+require_once('custom/include/Email/sendmail.php');
 class AOR_ReportsViewWeeklyreport extends SugarView {
-	
+
 	public function __construct() {
 		parent::SugarView();
 	}
@@ -20,7 +20,7 @@ class AOR_ReportsViewWeeklyreport extends SugarView {
 		$vendorSql="SELECT u.name FROM te_utm u INNER JOIN  te_vendor_te_utm_1_c uvr ON u.id=uvr.te_vendor_te_utm_1te_utm_idb INNER JOIN te_vendor v ON uvr.te_vendor_te_utm_1te_vendor_ida=v.id WHERE v.id IN('".implode("','",$vendors)."') AND u.utm_status='Live'";
 		$vendorObj =$db->query($vendorSql);
 		$utms=array();
-		while($row =$db->fetchByAssoc($vendorObj)){ 
+		while($row =$db->fetchByAssoc($vendorObj)){
 			$utms[]=$row['name'];
 		}
 		return $utms;
@@ -30,24 +30,50 @@ class AOR_ReportsViewWeeklyreport extends SugarView {
 		$vendorSql="SELECT distinct(v.id),v.name,date(te_ba_batch.date_entered) as date_entered FROM te_ba_batch INNER JOIN te_utm u on u.te_ba_batch_id_c=te_ba_batch.id INNER JOIN  te_vendor_te_utm_1_c uvr ON u.id=uvr.te_vendor_te_utm_1te_utm_idb INNER JOIN te_vendor v ON uvr.te_vendor_te_utm_1te_vendor_ida=v.id WHERE te_ba_batch.id='".$batch."' AND u.utm_status='Live'";
 		$vendorObj =$db->query($vendorSql);
 		$vendorOptions=array();
-		while($row =$db->fetchByAssoc($vendorObj)){ 
+		while($row =$db->fetchByAssoc($vendorObj)){
 			$vendorOptions[]=$row;
 		}
 		return $vendorOptions;
 	}
-	function getActualPlanByBatch($batch_id,$week_date){
+	function getActualPlanByBatch($batch_id,$week_date,$week=NULL){
+		//echo $batch_id; print_r($week_date);exit();
 		global $db;
-		$actualPlanSql="SELECT sum(cpl) as cpl, sum(cpa) as cpa FROM te_actual_campaign WHERE te_ba_batch_id_c='".$batch_id."' AND deleted=0 AND date(date_entered) >='".$week_date['week_start']."' AND date(date_entered)<='".$week_date['week_end']."'";
-		$actualPlanObj =$db->query($actualPlanSql);
-		$actualPlan =$db->fetchByAssoc($actualPlanObj);
+		$query3="SELECT count(l.id)total_leads,count(l2.id)converted FROM leads l inner join leads_cstm lc ON l.id = lc.id_c AND lc.te_ba_batch_id_c='".$batch_id."' AND l.status IN('Alive','Warm','Dead','Converted','Dropout') AND l.deleted=0  left join leads l2 on l2.id=l.id AND l2.status='Converted' AND l2.deleted=0 WHERE l.date_entered >= '".$week_date['week_start']."' AND l.date_entered <= '".$week_date['week_end']."'";
+		//echo $query3;exit();
+		$row3 =$db->query($query3);
+		$reco3 =$db->fetchByAssoc($row3);
+
+		//$actualPlanSql="SELECT sum(cpl) as cpl, sum(cpa) as cpa FROM te_actual_campaign WHERE te_ba_batch_id_c='".$batch_id."' AND deleted=0 AND date(plan_date) >='".$week_date['week_start']."' AND date(plan_date)<='".$week_date['week_end']."'";
+		//echo $actualPlanSql;exit();
+		$query4="SELECT sum(t3.rate)rate,sum(t3.volume)volume,sum(t3.total_cost)total_cost FROM `te_ba_batch` INNER JOIN te_actual_campaign as t3 on t3.te_ba_batch_id_c=te_ba_batch.id AND t3.deleted=0 AND t3.type='paid' AND  t3.plan_date >= '".$week_date['week_start']."' AND t3.plan_date <= '".$week_date['week_end']."'";
+		$query4Obj =$db->query($query4);
+		$reco4 =$db->fetchByAssoc($query4Obj);
+
+
+		$actualPlan['cpl'] = $reco4['total_cost']/$reco3['total_leads'];
+		$actualPlan['cpa']= $reco4['total_cost']/$reco3['converted'];
+		$actualPlan['week']= $week;
 		return $actualPlan;
 	}
-	function getActualPlanByUtm($utms,$week_date){
+	function getActualPlanByUtm($utms,$week_date,$week=NULL,$vendor=NULL){
 		global $db;
-		$actualPlanSql="SELECT sum(cpl) as cpl, sum(cpa) as cpa FROM te_actual_campaign WHERE name IN ('".implode("','",$utms)."') AND deleted=0 AND date(date_entered) >='".$week_date['week_start']."' AND date(date_entered)<='".$week_date['week_end']."'";
-		$actualPlanObj =$db->query($actualPlanSql);
-		$actualPlan =$db->fetchByAssoc($actualPlanObj);
-		return $actualPlan;
+		if($utms){
+			$query3="SELECT count(l.id)total_leads,count(l2.id)converted FROM leads l inner join leads_cstm lc ON l.id = lc.id_c AND l.status IN('Alive','Warm','Dead','Converted','Dropout') AND l.deleted=0  AND l.utm in('".implode("','",$utms)."') left join leads l2 on l2.id=l.id AND l2.status='Converted' AND l2.deleted=0 WHERE l.date_entered >= '".$week_date['week_start']."' AND l.date_entered <= '".$week_date['week_end']."' ";
+			//echo $query3;exit();
+			$row3 =$db->query($query3);
+			$reco3 =$db->fetchByAssoc($row3);
+
+			$query4="SELECT sum(t3.rate)rate,sum(t3.volume)volume,sum(t3.total_cost)total_cost FROM `te_ba_batch` INNER JOIN te_actual_campaign as t3 on t3.te_ba_batch_id_c=te_ba_batch.id AND t3.deleted=0 AND t3.type='paid' AND t3.vendor_id in('".implode("','",$vendor)."') AND  t3.plan_date >= '".$week_date['week_start']."' AND t3.plan_date <= '".$week_date['week_end']."'";
+			$query4Obj =$db->query($query4);
+			$reco4 =$db->fetchByAssoc($query4Obj);
+
+
+			$actualPlan['cpl'] = $reco4['total_cost']/$reco3['total_leads'];
+			$actualPlan['cpa']= $reco4['total_cost']/$reco3['converted'];
+			$actualPlan['week']= $week;
+			return $actualPlan;
+		}
+
 	}
 	public function display() {
 		global $db;
@@ -57,7 +83,7 @@ class AOR_ReportsViewWeeklyreport extends SugarView {
 		$batchList=array();
 		while($row =$db->fetchByAssoc($batchObj)){
 			$batchList[]=$row;
-		}	
+		}
 		$selected_batch="";
 		$batch_created_date="";
 		$utms=array();
@@ -67,6 +93,10 @@ class AOR_ReportsViewWeeklyreport extends SugarView {
 			#Weeks of the batch created date and till date
 			$vendorList=$this->getVendors($_POST['batch_val']);
 			$utms=$this->getUtm($_REQUEST['vendor_val']);
+			//echo "<pre>";print_r($utms);print_r($_REQUEST['vendor_val']);exit();
+			if(isset($_REQUEST['vendor_val']) && !empty($_REQUEST['vendor_val'])){
+				$where = " AND (vendor_id IN('".implode("','",$_REQUEST['vendor_val'])."')) ";
+			}
 			$weeks=array();
 			$selected_batch=$_POST['batch_val'];
 			$batch_created_date=$_POST['batch_created_date'];
@@ -74,16 +104,28 @@ class AOR_ReportsViewWeeklyreport extends SugarView {
 			$y=$date[0];
 			$m=$date[1];
 			$d=$date[2];
-			for($x=0;$x<date('d');$x++){
-				$week=(Integer)date("W", mktime(0,0,0,$m,$d+$x,$y));
-				$weeks[$week]=$week;
-			}			
-			foreach($weeks as $key=>$value){
-				if(!empty($utms))
-					$reportData[$key]=$this->getActualPlanByUtm($utms,$this->getStartAndEndDate($value,$y));
-				else
-					$reportData[$key]=$this->getActualPlanByBatch($_POST['batch_val'],$this->getStartAndEndDate($value,$y));
+			$get_min_max_actual_date = "SELECT MIN(plan_date)min_date,MAX(plan_date)max_date FROM te_actual_campaign WHERE te_ba_batch_id_c='".$_POST['batch_val']."' AND deleted=0 $where";
+			$get_min_max_actual_dateObj = $db->query($get_min_max_actual_date);
+			$row_get_min_max_actual_date =$db->fetchByAssoc($get_min_max_actual_dateObj);
+
+			if($row_get_min_max_actual_date['min_date'] && $row_get_min_max_actual_date['max_date']){
+				$start = $row_get_min_max_actual_date['min_date'];
+				$end = $row_get_min_max_actual_date['max_date'];
+
+				$dates = range(strtotime($start), strtotime($end),604800);
+				$weeks = array_map(function($v){return date('W', $v);}, $dates);
+				foreach($weeks as $key=>$value){
+					if(!empty($utms)){
+						$reportData[$value]=$this->getActualPlanByUtm($utms,$this->getStartAndEndDate($value,$y),$value,$_REQUEST['vendor_val']);
+					}
+					else{
+						$reportData[$value]=$this->getActualPlanByBatch($_POST['batch_val'],$this->getStartAndEndDate($value,$y),$value);
+					}
+				}
+				//echo "<pre>";print_r($reportData);exit();
 			}
+
+
 		}elseif(isset($_POST['export']) && $_POST['export']=="Export"){
 			$data="Week,CPL,CPA\n";
 			$file = "weekly_report";
@@ -91,6 +133,9 @@ class AOR_ReportsViewWeeklyreport extends SugarView {
 			#Weeks of the batch created date and till date
 			$vendorList=$this->getVendors($_POST['batch_val']);
 			$utms=$this->getUtm($_REQUEST['vendor_val']);
+			if(isset($_REQUEST['vendor_val']) && !empty($_REQUEST['vendor_val'])){
+				$where = " AND (vendor_id IN('".implode("','",$_REQUEST['vendor_val'])."')) ";
+			}
 			$weeks=array();
 			$selected_batch=$_POST['batch_val'];
 			$batch_created_date=$_POST['batch_created_date'];
@@ -98,19 +143,34 @@ class AOR_ReportsViewWeeklyreport extends SugarView {
 			$y=$date[0];
 			$m=$date[1];
 			$d=$date[2];
-			for($x=0;$x<date('d');$x++){
-				$week=(Integer)date("W", mktime(0,0,0,$m,$d+$x,$y));
-				$weeks[$week]=$week;
-			} 
-			foreach($weeks as $key=>$value){				
-				if(!empty($utms))
-					$reportData=$this->getActualPlanByUtm($utms,$this->getStartAndEndDate($value,$y));
-				else
-					$reportData=$this->getActualPlanByBatch($_POST['batch_val'],$this->getStartAndEndDate($value,$y));
-				
-				$reportData['cpl']=$reportData['cpl']?number_format($reportData['cpl'],2):"0.00";
-				$reportData['cpa']=$reportData['cpa']?number_format($reportData['cpa'],2):"0.00";
-				$data.= "\"" . $key . "\",\"" . $reportData ['cpl'] . "\",\"" . $reportData ['cpa']. "\"\n";
+			$get_min_max_actual_date = "SELECT MIN(plan_date)min_date,MAX(plan_date)max_date FROM te_actual_campaign WHERE te_ba_batch_id_c='".$_POST['batch_val']."' AND deleted=0 $where";
+			$get_min_max_actual_dateObj = $db->query($get_min_max_actual_date);
+			$row_get_min_max_actual_date =$db->fetchByAssoc($get_min_max_actual_dateObj);
+
+			if($row_get_min_max_actual_date['min_date'] && $row_get_min_max_actual_date['max_date']){
+				$start = $row_get_min_max_actual_date['min_date'];
+				$end = $row_get_min_max_actual_date['max_date'];
+
+				$dates = range(strtotime($start), strtotime($end),604800);
+				$weeks = array_map(function($v){return date('W', $v);}, $dates);
+				foreach($weeks as $key=>$value){
+					if(!empty($utms)){
+						$reportData[$value]=$this->getActualPlanByUtm($utms,$this->getStartAndEndDate($value,$y),$value,$_REQUEST['vendor_val']);
+					}
+					else{
+						$reportData[$value]=$this->getActualPlanByBatch($_POST['batch_val'],$this->getStartAndEndDate($value,$y),$value);
+					}
+				}
+			}
+			foreach($reportData as $key=>$value){
+				if(!$value['cpl']){
+					$value['cpl']='0.00';
+				}
+				if(!$value['cpa']){
+					$value['cpa']='0.00';
+				}
+
+				$data.= "\"" . $key . "\",\"" . round($value['cpl'],2) . "\",\"" . round($value['cpa'],2). "\"\n";
 			}
 			ob_end_clean();
 			header("Content-type: application/csv");
@@ -125,9 +185,12 @@ class AOR_ReportsViewWeeklyreport extends SugarView {
 			</tr>';
 			$file = "weekly_report";
 			$filename = $file . "_" . date ( "Y-m-d");
-			#Weeks of the batch created date and till date
+
 			$vendorList=$this->getVendors($_POST['batch_val']);
 			$utms=$this->getUtm($_REQUEST['vendor_val']);
+			if(isset($_REQUEST['vendor_val']) && !empty($_REQUEST['vendor_val'])){
+				$where = " AND (vendor_id IN('".implode("','",$_REQUEST['vendor_val'])."')) ";
+			}
 			$weeks=array();
 			$selected_batch=$_POST['batch_val'];
 			$batch_created_date=$_POST['batch_created_date'];
@@ -135,76 +198,92 @@ class AOR_ReportsViewWeeklyreport extends SugarView {
 			$y=$date[0];
 			$m=$date[1];
 			$d=$date[2];
-			for($x=0;$x<date('d');$x++){
-				$week=(Integer)date("W", mktime(0,0,0,$m,$d+$x,$y));
-				$weeks[$week]=$week;
-			} 
-			foreach($weeks as $key=>$value){	
-				if(!empty($utms)){
-					$templateData=$this->getActualPlanByUtm($utms,$this->getStartAndEndDate($value,$y));
-					$reportData[$key]=$templateData;
-				}else{
-					$templateData=$this->getActualPlanByBatch($_POST['batch_val'],$this->getStartAndEndDate($value,$y));
-					$reportData[$key]=$templateData;
+			$get_min_max_actual_date = "SELECT MIN(plan_date)min_date,MAX(plan_date)max_date FROM te_actual_campaign WHERE te_ba_batch_id_c='".$_POST['batch_val']."' AND deleted=0 $where";
+			$get_min_max_actual_dateObj = $db->query($get_min_max_actual_date);
+			$row_get_min_max_actual_date =$db->fetchByAssoc($get_min_max_actual_dateObj);
+
+			if($row_get_min_max_actual_date['min_date'] && $row_get_min_max_actual_date['max_date']){
+				$start = $row_get_min_max_actual_date['min_date'];
+				$end = $row_get_min_max_actual_date['max_date'];
+
+				$dates = range(strtotime($start), strtotime($end),604800);
+				$weeks = array_map(function($v){return date('W', $v);}, $dates);
+				foreach($weeks as $key=>$value){
+					if(!empty($utms)){
+						$reportData[$value]=$this->getActualPlanByUtm($utms,$this->getStartAndEndDate($value,$y),$value,$_REQUEST['vendor_val']);
+					}
+					else{
+						$reportData[$value]=$this->getActualPlanByBatch($_POST['batch_val'],$this->getStartAndEndDate($value,$y),$value);
+					}
+				}
+			}
+			foreach($reportData as $key=>$value){
+				if(!$value['cpl']){
+					$value['cpl']='0.00';
+				}
+				if(!$value['cpa']){
+					$value['cpa']='0.00';
 				}
 				$template.='<tr height="20">
 				   <td align="left" valign="top" >'.$key.'</td>
-				   <td align="left" valign="top" >'.number_format($templateData['cpl'],2).'</td> 
-				   <td align="left" valign="top">'.number_format($templateData['cpa'],2).'</td>					
+				   <td align="left" valign="top" >'.number_format($value['cpl'],2).'</td>
+				   <td align="left" valign="top">'.number_format($value['cpa'],2).'</td>
 				</tr>';
 			}
+
 			$template.="</table>";
 			$recipientsSql="SELECT name,email,report FROM te_report_recipients WHERE deleted=0 AND report='Weekly'";
 			$recipientsObj =$db->query($recipientsSql);
 			$recipients="";
 			while($row =$db->fetchByAssoc($recipientsObj)){
 				$recipients.=$row['email'].",";
-			}			
+			}
 			if($recipients!=""){
 				$recipients=substr($recipients,0,-1);
-				$mail = new NetCoreEmail();			
+				$mail = new NetCoreEmail();
 				$mail->sendEmail($recipients,"Weekly Report",$template);
-			}			
-		}	
-#PS @Manish	
-			$total=count($reportData); #total records		
-					
-			$start=0;		
-			$per_page=10;		
+			}
+		}
+#PS @Manish
+			$total=count($reportData); #total records
+
+			$start=0;
+			$per_page=10;
 			$page=1;
-			$pagenext=1;		
-			$last_page=ceil($total/$per_page);		
-					
-			if(isset($_REQUEST['page'])&&$_REQUEST['page']>0){		
-				$start=$per_page*($_REQUEST['page']-1);		
+			$pagenext=1;
+			$last_page=ceil($total/$per_page);
+
+			if(isset($_REQUEST['page'])&&$_REQUEST['page']>0){
+				$start=$per_page*($_REQUEST['page']-1);
 				$page=($_REQUEST['page']-1);
 				$pagenext = ($_REQUEST['page']+1);
-							
-			}else{		
+
+			}else{
 				//$page++;
-				$pagenext++;		
-			}				
-			if(($start+$per_page)<$total){					
-				$right=1;		
-			}else{		
-				$right=0;		
-			}		
-			if(isset($_REQUEST['page'])&&$_REQUEST['page']==1){		
-				$left=0;					
-			}elseif(isset($_REQUEST['page'])){		
-				//$page=($_REQUEST['page']-1);		
-				$left=1;		
+				$pagenext++;
 			}
-						
-			$reportData=array_slice($reportData,$start,$per_page);		
-			if($total>$per_page){		
-				$current="(".($start+1)."-".($start+$per_page)." of ".$total.")";	
-					
-			}else{		
+			if(($start+$per_page)<$total){
+				$right=1;
+			}else{
+				$right=0;
+			}
+			if(isset($_REQUEST['page'])&&$_REQUEST['page']==1){
+				$left=0;
+			}elseif(isset($_REQUEST['page'])){
+				//$page=($_REQUEST['page']-1);
+				$left=1;
+			}
+
+			$reportData=array_slice($reportData,$start,$per_page);
+			if($total>$per_page){
+				$current="(".($start+1)."-".($start+$per_page)." of ".$total.")";
+
+			}else{
 				$current="(".($start+1)."-".count($reportData)." of ".$total.")";
-			
-			}		
+
+			}
 		#pE
+		//echo "<pre>";print_r($reportData);exit();
 		$sugarSmarty = new Sugar_Smarty();
 		$sugarSmarty->assign("batchList",$batchList);
 		$sugarSmarty->assign("vendorList",$vendorList);
@@ -212,11 +291,11 @@ class AOR_ReportsViewWeeklyreport extends SugarView {
 		$sugarSmarty->assign("selected_batch",$selected_batch);
 		$sugarSmarty->assign("batch_created_date",$batch_created_date);
 		$sugarSmarty->assign("selected_vendor",$_REQUEST['vendor_val']);
-		$sugarSmarty->assign("current_records",$current);		
-		$sugarSmarty->assign("page",$page);	
-		$sugarSmarty->assign("pagenext",$pagenext);		
-		$sugarSmarty->assign("right",$right);		
-		$sugarSmarty->assign("left",$left);		
+		$sugarSmarty->assign("current_records",$current);
+		$sugarSmarty->assign("page",$page);
+		$sugarSmarty->assign("pagenext",$pagenext);
+		$sugarSmarty->assign("right",$right);
+		$sugarSmarty->assign("left",$left);
 		$sugarSmarty->assign("last_page",$last_page);
 		$sugarSmarty->display('custom/modules/AOR_Reports/tpls/weeklyreport.tpl');
 	}
