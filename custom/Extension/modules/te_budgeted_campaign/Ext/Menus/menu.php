557ed0aada821241cc35b24d7c2412e6b9826181
mranupkmr <mr.anupkmr@gmail.com>
2017-06-19 12:03:21 +0530

    Budgeted VS Actual removed duplicate link

--- custom/Extension/modules/te_budgeted_campaign/Ext/Menus/menu.php
+++ custom/Extension/modules/te_budgeted_campaign/Ext/Menus/menu.php
@@ -5,6 +5,6 @@ global $mod_strings, $app_strings, $sugar_config;
 unset($module_menu[0]);
 unset($module_menu[1]);
 if(ACLController::checkAccess('te_budgeted_campaign', 'import', true))$module_menu[]=Array("index.php?module=te_budgeted_campaign&action=budget_summary", "Budgeted Campaign Plan", "");
-if(ACLController::checkAccess('te_budgeted_campaign', 'import', true))$module_menu[]=Array("index.php?module=te_budgeted_campaign&action=budgeted_actual", "Budgeted Vs Actual", "");
+
 
 ?>
--- custom/modules/te_budgeted_campaign/budgeted_actual.php
+++ custom/modules/te_budgeted_campaign/budgeted_actual.php
@@ -1,5 +1,5 @@
 <?php
-/* This is Custom file  For Budgeted vs Compaign 
+/* This is Custom file  For Budgeted vs Compaign
  * Display menu from Action at Module te_budgeted campaign
  *  Created date -02-dec-2016 @Manish Gupta 9650211216
  * */
@@ -37,24 +37,24 @@ input[type=text], select {
                 <fieldset>
                     <form action="index.php?module=te_budgeted_campaign&action=budgeted_actual&search_query=1" method="post" id="lead_trans">
                       <h2>Budgeted Vs Actual Report</h2><br/><br/>
-                      
+
                       <table>
 						  <tr>
-					<?php 										  									
+					<?php
             	     //@ Get All Batches for dropdown 1
 							  $fetch="SELECT id,name FROM te_ba_batch WHERE batch_status='enrollment_in_progress' AND deleted=0";
                               $row =$db->query($fetch);?>
-							  
+
 							  <td><b>Select Batch</b></td>
 							   <td>
 								   <!--onChange="this.form.submit()"-->
 								  <select name="batch_val" id="batch_val">
 								<option  value="">Select Batch</option>
-									
+
 									<?php while($records =$db->fetchByAssoc($row)){
 									      //$_SESSION['name1']=$records['name'];
-									 
-									 ?> 
+
+									 ?>
                                   <option  value="<?php echo $records['id']?>"><?php echo $records['name']?></option>
                                   <?php }?>
                                 </select>
@@ -63,31 +63,31 @@ input[type=text], select {
 									<td>
 								<select name="utm_val[]"  multiple id="utm_val"></select>
 							    </td>
-									<td><b>Select Week</b> </td>							   						   
+									<td><b>Select Week</b> </td>
 								<td>
 									<select name="week" id="week_val">
 									<option value="">--Select Week--</option>
 								<?php
-								for($count = 1; $count<=52; $count++){ 
-									
+								for($count = 1; $count<=52; $count++){
+
 								?>
 									<option  value="<?php echo $count;?>"><?php echo $count; ?></option>
-								<?php  } ?>                               
+								<?php  } ?>
                                 </select></td>
-							  
+
 							    </tr>
 							    	<tr> <td align='center' style="border:none;"><input type="Submit" name="Submit" value="Find Report"></td>
 							     </tr>
-							     </table>                         
-									</form>                                    
-										</fieldset>      			
+							     </table>
+									</form>
+										</fieldset>
 										</section>
-            
+
 
 					<?php
 						if(isset($_POST['Submit']) &&!empty($_POST['Submit'])) {
 						extract($_POST);
-									     
+
 					function getStartAndEndDate($week, $year)
 					{
 
@@ -100,16 +100,16 @@ input[type=text], select {
 						$return[1] = date('Y-m-d', $time);
 						$return[1] = $return[1]." 00:00:00";
 						return $return;
-					}  
-												 
-						     
+					}
+
+
  //@Get Budgeted Lead,Coverted Lead,rate,volume,cpl,cpa,Conversion Rate,Volume,Cost by batch 9
-									
+
 									$where='';
 									$whereActual='';
 									$whereUtm='';
 									$whereActual1='';
-									
+
 									if(count($_POST['utm_val'])>0 && !empty($_POST['week'])){
 									 $where=' WHERE ';
 									 $whereActual=' WHERE ';
@@ -140,58 +140,58 @@ input[type=text], select {
 									 $where .="  t4.week='".$_POST['week']."'";
 									 $whereActual .= "  l.date_entered >= '".$weekdata[0]."' AND l.date_entered <= '".$weekdata[1]."'";
 									 $whereActual1 .= "  t3.plan_date >= '".$weekdata[0]."' AND t3.plan_date <= '".$weekdata[1]."'";
-									 
-									 
+
+
 									}
-									
+
 													$query9="SELECT sum(t4.leads)leads,sum(t4.conversion)conversion,sum(t4.conversion_rate)conversion_rate,sum(t4.clp)cpl,sum(t4.cpa)cpa,sum(t4.volume)volume,sum(t4.cost)cost FROM `te_ba_batch` INNER JOIN te_utm on te_utm.te_ba_batch_id_c=te_ba_batch.id AND te_ba_batch.id='".$_REQUEST['batch_val']."' AND te_utm.utm_status='Live'INNER JOIN te_utm_te_budgeted_campaign_1_c AS t3 on t3.te_utm_te_budgeted_campaign_1te_utm_ida=te_utm.id
 													INNER JOIN te_budgeted_campaign AS t4 on t4.id=t3.te_utm_te_budgeted_campaign_1te_budgeted_campaign_idb AND t4.deleted=0 $where";
 													$row9 =$db->query($query9);
 													$reco9 =$db->fetchByAssoc($row9);
-													
-													
+
+
 						//@Get Actual Lead,Converted Lead Based on Batch 3
-                                
+
 										$query3="SELECT count(l.id)total_leads,count(l2.id)converted FROM leads l inner join leads_cstm lc ON l.id = lc.id_c AND lc.te_ba_batch_id_c='".$_REQUEST['batch_val']."' left join leads l2 on l2.id=l.id AND l2.status='Converted' $whereActual";
                                         $row3 =$db->query($query3);
             	                        $reco3 =$db->fetchByAssoc($row3);
-            	                  
+
 									//	echo "Total Leads".$reco3['total_leads'].'<br>';
 										//echo "Total Converted".$reco3['converted'];
-												
+
 						//@Get Actual Rate,Volume,Total Cost 4
-                                
+
 										$query4="SELECT sum(t3.rate)rate,sum(t3.volume)volume,sum(t3.total_cost)total_cost FROM `te_ba_batch` INNER JOIN te_utm t1 on t1.te_ba_batch_id_c=te_ba_batch.id AND te_ba_batch.id='".$_REQUEST['batch_val']."'AND t1.utm_status='Live' INNER JOIN te_utm_te_actual_campaign_1_c AS t2 on t2.te_utm_te_actual_campaign_1te_utm_ida=t1.id INNER JOIN te_actual_campaign as t3 on t3.id=t2.te_utm_te_actual_campaign_1te_actual_campaign_idb AND t3.deleted=0 AND t3.type='paid' $whereActual1";
                                         $row4 =$db->query($query4);
             	                        $reco4 =$db->fetchByAssoc($row4);
             	                        $cpl = $reco4['total_cost']/$reco3['total_leads'];
-            	                        $cpa= $reco4['total_cost']/$reco3['converted']; 
-            	                        $cr= $reco3['converted']/$reco3['total_leads']*100;            
+            	                        $cpa= $reco4['total_cost']/$reco3['converted'];
+            	                        $cr= $reco3['converted']/$reco3['total_leads']*100;
 										// echo "Rate".'<b>'.$reco4['rate'].'<br>';
 										// echo "Volume".$reco4['volume'].'<br>';
 										// echo "Cost".$reco4['total_cost'].'<br>';
-            	                   						     
+
 	         $fetcht="SELECT id,name FROM te_ba_batch WHERE batch_status='enrollment_in_progress' And id='".$_REQUEST['batch_val']."'";
              $rowfill =$db->query($fetcht);
              $filler =$db->fetchByAssoc($rowfill);
              if(empty($filler['name'])){
-				 
+
 				 //echo "<h3><b> Please Select Batch</h3></b>";
 		 echo '<script>';
-         echo "alert('Please Select Batch')";  
-         echo '</script>'; 
+         echo "alert('Please Select Batch')";
+         echo '</script>';
 				 }
-				 else{			
+				 else{
 						echo " Search Result For Batch-:<h3><b> ".$filler['name']."</h3></b>";
-					
+
 	if(count($_POST['utm_val'])>0){
-		
+
 	echo "UTM-<h3><b>".str_replace("'","",$whereUtm)."</h3></b>";
 }
-?>						
+?>
 
 	<table width="100%" style="padding:0px;" border="0" cellpadding="0" cellspacing="0">
-		
+
   <tr>
     <td style="border:0px; padding:0px;"  valign="top"><table width="100%" border="0" cellpadding="0" cellspacing="0">
       <tr>
@@ -210,7 +210,7 @@ input[type=text], select {
         <th>CPA</th>
       </tr>
       <tr>
-        <th>Conversion Rate</th>
+      <th>Conversion Rate(%)</th>
       </tr>
       <tr>
         <th>Total Cost</th>
@@ -218,7 +218,7 @@ input[type=text], select {
       <tr>
         <th>Volume</th>
       </tr>
-      
+
     </table></td>
 
     <td style="border:0px; padding:0px;" valign="top"><table width="100%" border="0" cellpadding="0" cellspacing="0">
@@ -229,27 +229,28 @@ input[type=text], select {
         <td style="padding:0px;  border:0px;"  ><table width="100%" border="0" cellpadding="0" cellspacing="0">
           <tr>
             <td><?php if($reco9['leads']==''){
-				echo 0;} else { echo $reco9['leads']; } ?>&nbsp;</td>
+				echo 0;} else { echo round($reco9['leads']); } ?>&nbsp;</td>
           </tr>
           <tr>
-            <td><?php if($reco9['conversion']==''){echo 0;}else {echo $reco9['conversion'];}?>&nbsp;</td>
+            <td><?php if($reco9['conversion']==''){echo 0;}else {echo round($reco9['conversion']);}?>&nbsp;</td>
+          </tr>
+					<tr>
+            <td><?php if($reco9['cpl']==''){echo 0;}else {echo round($reco9['cpl'],2);}?>&nbsp;</td>
           </tr>
           <tr>
-            <td><?php if($reco9['conversion_rate']==''){echo 0;}else {echo $reco9['conversion_rate'];}?>&nbsp;</td>
+            <td><?php if($reco9['cpa']==''){echo 0;}else { echo round($reco9['cpa'],2);}?>&nbsp;</td>
           </tr>
           <tr>
-            <td><?php if($reco9['cpl']==''){echo 0;}else {echo $reco9['cpl'];}?>&nbsp;</td>
+						<td><?php if($reco9['conversion_rate']==''){echo 0;}else {echo round($reco9['conversion_rate'],2);}?>&nbsp;</td>
+
           </tr>
           <tr>
-            <td><?php if($reco9['cpa']==''){echo 0;}else { echo $reco9['cpa'];}?>&nbsp;</td>
+            <td><?php if($reco9['cost']==''){echo 0;} else{echo round($reco9['cost'],2);}?>&nbsp;</td>
           </tr>
           <tr>
-            <td><?php if($reco9['cost']==''){echo 0;} else{echo $reco9['cost'];}?>&nbsp;</td>
+            <td><?php if($reco9['volume']==''){echo 0;}else {echo round($reco9['volume'],2);}?>&nbsp;</td>
           </tr>
-          <tr>
-            <td><?php if($reco9['volume']==''){echo 0;}else {echo $reco9['volume'];}?>&nbsp;</td>
-          </tr>
-          
+
         </table></td>
       </tr>
     </table></td>
@@ -258,23 +259,23 @@ input[type=text], select {
         <th>Actual</th>
       </tr>
       <tr>
-		 	  
+
         <td  valign="top" style="padding:0px; border:0px;" ><table width="100%" border="0" cellpadding="0" cellspacing="0">
-       
+
           <tr>
            <td><?php if($reco3['total_leads']==''){echo 0;} else {echo $reco3['total_leads'];}?>&nbsp;</td>
           </tr>
           <tr>
            <td><?php if($reco3['converted']==''){echo 0;} else {echo $reco3['converted'];}?>&nbsp;</td>
           </tr>
-          <tr>
-            <td><?php echo round($cpl,3);?>&nbsp;</td>
+					<tr>
+            <td><?php echo round($cpl,2);?>&nbsp;</td>
           </tr>
           <tr>
-            <td><?php echo $cpa;?>&nbsp;</td>
+            <td><?php echo round($cpa,2);?>&nbsp;</td>
           </tr>
           <tr>
-            <td><?php echo round($cr,3);?>&nbsp;</td>
+            <td><?php echo round($cr,2);?>&nbsp;</td>
           </tr>
           <tr>
             <td><?php if($reco4['total_cost']==''){ echo 0;} else {echo $reco4['total_cost'];}?>&nbsp;</td>
@@ -282,7 +283,7 @@ input[type=text], select {
             <tr>
             <td><?php if($reco4['volume']==''){ echo 0;} else {echo $reco4['volume'];}?>&nbsp;</td>
           </tr>
-         
+
         </table></td>
       </tr>
     </table></td>
@@ -293,9 +294,9 @@ input[type=text], select {
 	<?php
   }
     }
-					
+
 	?>
-		
+
 	<script>
 $(function(){
 	$("#batch_val").change(function(){
