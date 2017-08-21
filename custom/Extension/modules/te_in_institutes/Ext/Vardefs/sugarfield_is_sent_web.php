a01fced3ba4c3cdc4550ec4e3949e117dfda4e66
pawan <pawan.kumar@talentedge.in>
2017-08-21 15:08:09 +0530

    added for SAP report

--- custom/Extension/modules/te_in_institutes/Ext/Vardefs/sugarfield_is_sent_web.php
+++ custom/Extension/modules/te_in_institutes/Ext/Vardefs/sugarfield_is_sent_web.php
@@ -16,4 +16,21 @@ $dictionary['te_in_institutes']['fields']['is_sent_web'] =array (
 		'size' => '50',
 	);
 
+$dictionary['te_in_institutes']['fields']['SAP_Status'] =array (
+               'name' => 'SAP_Status',
+               'vname' => 'SAP Status',
+               'type' => 'int',
+               'required' => false,
+               'comments' => 'to check whether institute is downloaded to SAP',
+               'help' => '',
+               'default'=> 0,
+               'importable' => 'false',
+               'duplicate_merge' => 'disabled',
+               'duplicate_merge_dom_value' => '0',
+               'audited' => false,
+               'reportable' => false,
+               'len' => '50',
+               'size' => '50',
+       );
+
 ?>
