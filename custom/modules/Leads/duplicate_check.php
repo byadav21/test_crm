1b1a3fef8532e4ce45a02a6b27ae3add1529bc3c
anup.kumar@engenia.in <anup.kumar@engenia.in>
2017-10-16 17:29:54 +0530

    Update lead duplicate check

--- custom/modules/Leads/duplicate_check.php
+++ custom/modules/Leads/duplicate_check.php
@@ -7,7 +7,6 @@ if (!defined('sugarEntry') || !sugarEntry)die('Not A Valid Entry Point');
 	$sql = "SELECT GROUP_CONCAT(DISTINCT(l.`id`) ORDER BY l.date_entered)lead_id,count(l.id)total FROM `leads` AS l INNER JOIN leads_cstm lc ON l.id=lc.id_c LEFT JOIN email_addr_bean_rel el ON l.id = el.bean_id LEFT JOIN email_addresses e ON el.email_address_id = e.id WHERE l.deleted=0 AND l.status!='Duplicate'   GROUP BY  l.phone_other ,  l.`phone_mobile`,lc.te_ba_batch_id_c,e.email_address HAVING (COUNT(l.`phone_other`)>1 or COUNT(l.`phone_mobile`)>1 OR (COUNT(e.email_address)>1)) AND (COUNT(lc.te_ba_batch_id_c)>1) LIMIT 0,10";
 	$row = $db->query($sql);
 		if($row->num_rows>0){
-			//echo "Hello";exit;
 			$res='';
 			while($records =$db->fetchByAssoc($row)){
 				$res[] =$records;
@@ -31,7 +30,7 @@ if (!defined('sugarEntry') || !sugarEntry)die('Not A Valid Entry Point');
 					//$conn->query($insert_leads.implode(',',$lvaldata));
 					if($res_insert_leads==1){
 						$leads_id_for_update = '"'.implode('","', $leads).'"';
-						$update_leads_mark_duplicate = "UPDATE leads SET status='Duplicate',status_description='Duplicate',`duplicate_check`=1 WHERE id IN($leads_id_for_update)";
+						$update_leads_mark_duplicate = "UPDATE leads SET status='Warm',status_description='Re-Enquired',`duplicate_check`=1 WHERE id IN($leads_id_for_update)";
 						$res_update_leads =  $db->query($update_leads_mark_duplicate);
 						//$conn->query($update_leads_mark_duplicate);
 						$update_parent_lead = "UPDATE leads SET `duplicate_check`=1 WHERE id='".$parent_lead_id."'";
