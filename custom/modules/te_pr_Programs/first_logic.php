20dc872f3545ec34a8bd31a02818a91507e28e62
manish.kumar@engenia.in <manish.kumar@engenia.in>
2017-11-08 14:39:52 +0530

    progAPI

--- custom/modules/te_pr_Programs/first_logic.php
+++ custom/modules/te_pr_Programs/first_logic.php
@@ -33,7 +33,7 @@ class first_logic{
 				/*Insert program api call here*/
 				$user = 'talentedgeadmin';
 				$password = 'Inkoniq@2016';
-				$url = 'http://webstaging.talentedge.in/programme-api/';
+				$url = 'http://talentedge.staging.wpengine.com/programme-api/';
 				$headers = array(
 						'Authorization: Basic '. base64_encode("$user:$password")
 				);
@@ -65,7 +65,7 @@ class first_logic{
 				/*update program api call here*/
 				$user = 'talentedgeadmin';
 				$password = 'Inkoniq@2016';
-				$url = 'http://webstaging.talentedge.in/programme-api/';
+				$url = 'http://talentedge.staging.wpengine.com/programme-api/';
 				$headers = array(
 						'Authorization: Basic '. base64_encode("$user:$password")
 				);
