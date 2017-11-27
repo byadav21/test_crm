2f5564b045b73b2644d59da02dcaf4b9b392fd19
manish <manish.kumar@engenia.in>
2017-11-27 13:18:14 +0530

    crm lead search srm/sre condition add

--- custom/modules/Leads/search_leads.php
+++ custom/modules/Leads/search_leads.php
@@ -6,6 +6,7 @@
 	require_once('modules/ACLRoles/ACLRole.php');
 	global $db; 
 	global $current_user;
+	$Us=$current_user->id;
 	$acl_obj = new ACLRole();
 	$misData=$acl_obj->getUserSlug($current_user->id);
 	if($misData['slug']=='SRM' || $misData['slug']=='SRE'){
@@ -111,7 +112,7 @@ if(isset($_REQUEST['Search'])) {
         }
         
         
-                 echo  $fetch="SELECT tbl1.id,tbl1.salutation,tbl1.phone_mobile,tbl4.email_address,tbl1.assigned_user_id,tbl1.first_name,tbl1.last_name, tbl1.status,tbl1.date_entered,tbl1.lead_source,tbl2.user_name,tbl2.phone_mobile FROM leads AS tbl1
+					$fetch="SELECT tbl1.id,tbl1.salutation,tbl1.phone_mobile,tbl4.email_address,tbl1.assigned_user_id,tbl1.first_name,tbl1.last_name, tbl1.status,tbl1.date_entered,tbl1.lead_source,tbl2.user_name,tbl2.phone_mobile FROM leads AS tbl1
 							LEFT JOIN users AS tbl2 ON tbl1.assigned_user_id = tbl2.id INNER JOIN email_addr_bean_rel AS tbl3 ON tbl1.id=tbl3.bean_id INNER JOIN email_addresses AS tbl4 ON tbl3.email_address_id=tbl4.id WHERE ".$where;
         
         $row = $db->query($fetch);
