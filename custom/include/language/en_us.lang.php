10388799b51d4d279a7b38002be51f1d1a94a53d
Pankaj <pankaj.jha@talentedge.in>
2017-12-22 13:31:59 +0000

    added source OO_AG

--- custom/include/language/en_us.lang.php
+++ custom/include/language/en_us.lang.php
@@ -347,6 +347,7 @@ $GLOBALS['app_list_strings']['lead_source_custom_dom']=array (
   'OO_NAG'=>'TBA NAGPUR',
   'OO_GGN'=>'TBA GGN 2',
   'OO_CHN'=>'Channels',
+  'OO_AG' =>'Avenue Growth',
 
 );
 $app_list_strings['moduleList']['te_ExpensePO']='Expense Vouchers';
