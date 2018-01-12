5a24477bb6391cbfe058c568941505d644b7e7f0
anup.kumar@engenia.in <anup.kumar@engenia.in>
2018-01-12 16:05:22 +0530

    short name added

---
+++ custom/Extension/modules/te_in_institutes/Ext/Vardefs/sugarfield_short_name.php
@@ -0,0 +1,21 @@
+<?php
+
+$dictionary['te_in_institutes']['fields']['short_name'] =array (
+		'name' => 'short_name',
+		'vname' => 'Short Name',
+		'type' => 'varchar',
+		'required' => true,
+		'massupdate' => 1,
+		'comments' => '',
+		'help' => '',
+		'default'=>'',
+		'importable' => 'true',
+		'duplicate_merge' => 'disabled',
+		'duplicate_merge_dom_value' => '0',
+		'audited' => false,
+		'reportable' => true,
+		'len' => '35',
+		'size' => '35',
+	);
+
+?>
