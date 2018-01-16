24a27923afd101706d1829d075722f28f9a0207f
pawan <pawan.kumar@talentedge.in>
2018-01-16 12:21:25 +0530

    invoice api v.0.2

--- custom/modules/te_Api/web_lead_invoice.php
+++ custom/modules/te_Api/web_lead_invoice.php
@@ -8,29 +8,13 @@ $data = json_decode(file_get_contents('php://input'), true);
 $error_fields=[];
 $discount=' 0';
 
-
- $fp = fopen('invoice_API_log_.txt', 'a'); 
-            fwrite($fp, print_r($cResponse, TRUE)); 
-            fclose($fp);
             
-if(!isset($data['action']) || empty($data['action'])){
-	$error_fields['action']=['action field is required.'];
+            
+if(empty($data)){
+	$error_fields['error:']=['empty data'];
 
 }
-else{
-	/*if(!isset($data['crm_lead_id']) || empty($data['crm_lead_id'])){
-		$error_fields['crm_lead_id']=['crm_lead_id field is required.'];
-	}*/
-	if(!isset($data['email']) || empty($data['email'])){
-		$error_fields['email']=['email field is required.'];
-	}
-	
-	if($data['action']=='update'){
-		if(!isset($data['crm_payment_id']) || empty($data['crm_payment_id'])){
-			$error_fields['crm_payment_id']=['crm_payment_id field is required.'];
-		}
-	}
-}
+
 
 if($error_fields){
 	$response_result = array('status' => '400','result' => $error_fields);
@@ -38,23 +22,32 @@ if($error_fields){
 	exit();
 }
 else{
-	$lead_id=$data['crm_lead_id'];
-	$batch_id=$data['batch_crm_id'];
-	$email=$data['email'];
-	$mobile=$data['mobile'];
+    
+    
+      foreach($data as $key=>$val){
+          
+          
+        $crm_orderid=$val['crm_orderid'];
+        
+        if(!$crm_orderid){  continue; }
 
 	/*check valid crm_payment_id in case of update*/
-	if($data['action']=='update'){
-		$check_payment_sql = "SELECT p.* FROM `leads_te_payment_details_1_c` AS lpr INNER JOIN te_payment_details AS p ON p.id=lpr.`leads_te_payment_details_1te_payment_details_idb` WHERE lpr.`leads_te_payment_details_1te_payment_details_idb` ='".$data['crm_payment_id']."'";
-		$check_payment_Obj= $GLOBALS['db']->query($check_payment_sql);
-		$check_payment_row=$GLOBALS['db']->fetchByAssoc($check_payment_Obj);
-		if(!$check_payment_row){
-			$errors=array('type'=>'Invalid crm_payment_id');
-			$response_result = array('status' => '0','result' => $errors);
-			echo json_encode($response_result);
-			exit();
-		}
-	}
+	if($crm_orderid!='')
+            {
+            
+            
+                $GLOBALS['db']->query("UPDATE te_payment_details SET invoice_number='".$val['invno']."' WHERE id='".$crm_orderid."'");
+            
+                $fp = fopen('invoiceNo2_API_log_.txt', 'a'); 
+                fwrite($fp,$val); 
+                fclose($fp);
 	
-
+            }
+	
+      }
+    
+    
+	
+	
+}
 
