d508f34ed8cc3e016d74d635342555ed7d8b8a8e
pawan <pawan.kumar@talentedge.in>
2018-01-29 12:37:32 +0530

    added more email in report

--- custom/modules/te_Api/vendorwiseleadsreport_cron.php
+++ custom/modules/te_Api/vendorwiseleadsreport_cron.php
@@ -4,10 +4,10 @@ if (!defined('sugarEntry') || !sugarEntry)
     die('Not A Valid Entry Point');
 ini_set('memory_limit', '1024M');
 require_once('include/entryPoint.php');
-require_once('custom/include/Email/sendmail.php');
+require_once('custom/include/Email/sendmail_cron.php');
 global $db;
-//error_reporting(-1);
-//ini_set('display_errors', 'On');
+error_reporting(-1);
+ini_set('display_errors', 'On');
 
 class sendVisitReport
 {
@@ -21,77 +21,10 @@ class sendVisitReport
         $this->toDate   = date('Y-m-d');
     }
 
-    public function sendEmail($sentTo, $emailSubject, $emailMessage, $emailFromName = NULL, $emailFrom = NULL, $attachData = array())
-    {
-        $sentTo                = (!is_array($sentTo)) ? array($sentTo) : $sentTo;
-        // $sentTo = "virendra.bhardwaj@talentedge.in;
-        $data                  = array();
-        $data['api_key']       = 'fbb5606b326850fce2fa335cdce8dc16';
-        $data['email_details'] = array(
-            'fromname' => rawurlencode("Talentedge"),
-            'subject'  => rawurlencode($emailSubject),
-            'from'     => "notifications@talentedge.in",
-            'content'  => rawurlencode($emailMessage),
-        );
-        $data['recipients']    = $sentTo;
-        if (!empty($attachData))
-        {
-            $data['files'] = $attachData;
-        }
-
-        $data = array('data' => json_encode($data));
-
-        $url = "http://api.falconide.com/falconapi/web.send.json";
-        $ch  = curl_init();
-        curl_setopt($ch, CURLOPT_URL, $url);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-        curl_setopt($ch, CURLOPT_FAILONERROR, 1);
-        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($ch, CURLOPT_TIMEOUT, 0);
-        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 0);
-
-        curl_setopt($ch, CURLOPT_POST, 1);
-        curl_setopt($ch, CURLOPT_RANGE, "1-2000000");
-
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
-        curl_setopt($ch, CURLOPT_REFERER, 'http://www.talentedge.in');
-        $result = curl_exec($ch);
-        $result = curl_error($ch) ? curl_error($ch) : $result;
-        curl_close($ch);
-        return $result;
-    }
-
-    public function sendCertificateEmail($emailData)
-    {
-
-        if (!empty($emailData))
-        {
-            $userId       = $emailData['userId'];
-            $sentTo       = $emailData['email'];
-            
-            //print_r($sentTo); die;
-            $emailSubject = $emailData['subject'];
-            $emailMessage = $emailData['email_message'];
-            $certFilePath = $emailData['certFilePath'];
-            $attachData   = array();
-
-            if (file_exists($certFilePath))
-            {    
-                $attachFile    = file_get_contents($certFilePath);
-                $attachName    = $emailData['pdfFileName'] . ".csv";
-                $attachContent = rawurlencode($attachFile);
-                $attachData    = array($attachName => $attachContent);
-
-                //$Notification = ClassRegistry::init('Notification');
-                echo $this->sendEmail($sentTo, $emailSubject, $emailMessage, NULL, NULL, $attachData);
-            }
-        }
-    }
-
     public function main()
     {
         global $sugar_config, $app_list_strings, $current_user, $db;
+        $mail = new FalconideEmail();
 
         $file     = "VendorWiseAllocation_report";
         $where    = '';
@@ -147,13 +80,13 @@ class sendVisitReport
 
 
         foreach ($programList as $key => $councelor)
-        {   
-            $toal  =0;
+        {
+            $toal = 0;
             $data .= "\"" . $councelor['name'];
             $data .= "\",\"" . $councelor['batch_code'];
             foreach ($VendorList as $key1 => $value)
             {
-                $converted = $programList[$key][$key1]['lead_count'];
+                $converted = isset($programList[$key][$key1]) ? $programList[$key][$key1]['lead_count'] : 0;
                 $data      .= "\",\"" . $converted;
                 $toal      += $converted;
             }
@@ -165,30 +98,23 @@ class sendVisitReport
         $fp = fopen($_SERVER['DOCUMENT_ROOT'] . "/reports/" . $filename . ".csv", "wb");
         fwrite($fp, $data);
         fclose($fp);
-        chmod($filename. ".csv", 0777); 
+        chmod($_SERVER['DOCUMENT_ROOT'] . "/reports/" . $filename . ".csv", 0777);
 
         ///var/www/htmlVendorWiseReport_report_2018-01-22.csv
-
-        $emailData = array('email'=>  array('pawan.kumar@talentedge.in','ashwani.sharma@talentedge.in','ajay.kumar@talentedge.in',
-                                            'pramod.singh@talentedge.in','pritam.dutta@talentedge.in','karan.bhatia@talentedge.in',
-                                            'amit.sati@talentedge.in','vivek.bathla@talentedge.in'),
-            'subject'       => 'Vendor Wise Allocation Report - ' . date("F d, Y", strtotime($this->toDate)),
-            'email_message' => '<p>Hi,</p> '
-            . '<p>Please find in here attached, vendor wise lead allocation report for "' . date("F d, Y", strtotime($this->toDate)) . '"</p>',
-            'pdfFileName'   => $filename,
-            'certFilePath'  => $_SERVER['DOCUMENT_ROOT'] . "/reports/" . $filename . ".csv");
-
-        $this->sendCertificateEmail($emailData);
+        $emailData = $mail->emailData('Vendor Wise Lead Allocation Report', $filename, $this->toDate);
+        $mail->sendCertificateEmail($emailData);
     }
 
 }
 
-$mainObj = new sendVisitReport();
-
+$mainObj           = new sendVisitReport();
+//$mainObj->toDate   = '2017-11-04';
+//$mainObj->fromDate = '2017-11-04';
 if (strtotime($mainObj->fromDate) == strtotime($mainObj->toDate))
 {
     $fromDate = date('Y-m-d', (strtotime('-1 day', strtotime($mainObj->fromDate))));
 }
+
 $mainObj->toDate   = $fromDate;
 $mainObj->fromDate = $fromDate;
 
