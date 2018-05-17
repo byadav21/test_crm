40f56b29cc1971915e4a8f70035ff6158e3d9be5
pawan kumar <pawan.kumar@talentedge.in>
2018-05-17 16:57:15 +0530

    ameyo wo updater

--- custom/modules/te_Api/push_eloquascoreleadsto_ameyo.php
+++ custom/modules/te_Api/push_eloquascoreleadsto_ameyo.php
@@ -1,16 +1,14 @@
 <?php
 	if (!defined('sugarEntry') || !sugarEntry)die('Not A Valid Entry Point');
 global $db;
-echo "hii";
-
 $query="SELECT lead.id, batch.d_campaign_id, batch.d_lead_id from `leads_cstm`  as `leadcstm`
   INNER JOIN `te_ba_batch` as `batch` on leadcstm.te_ba_batch_id_c = batch.id
   INNER JOIN `leads` as `lead` on leadcstm.id_c = lead.id
   where leadcstm.eloqua_lead_score IN ('A1', 'A2', 'A3', 'B1', 'B2')
   and lead.status_description IN ('Fallout', 'Not interested', 'Retired', 'Cross Sell', 'Not Enquired', 'Next Batch')
   and batch.batch_status = 'enrollment_in_progress'
-	and lead.autoassign='Yes'
   and lead.deleted = '0'
+	and lead.update_flag = '0'
   group by leadcstm.te_ba_batch_id_c, lead.phone_mobile, leadcstm.email_add_c
   ORDER BY lead.date_entered ASC
   limit 1";
@@ -20,9 +18,11 @@ $query="SELECT lead.id, batch.d_campaign_id, batch.d_lead_id from `leads_cstm`  
 		$leadid=$row['id'];
 		$d_campaign_id=$row['d_campaign_id'];
 		$d_lead_id=$row['d_lead_id'];
-    $querylead="SELECT * from `leads` WHERE `id`='$leadid'   limit 1";
+    $querylead="SELECT * from `leads` WHERE `id`='$leadid' ";
     $resultlead = $db->Query($querylead);
     while ($rowlead =$db->fetchByAssoc($resultlead)){
+		//	echo "<pre>";
+		//	print_r($rowlead);
       $autoassign=$rowlead['autoassign'];
       $status=$rowlead['status'];
       $status_description=$rowlead['status_description'];
