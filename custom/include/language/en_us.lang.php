f072c639febac19a2e462a3f6733d94b3e317368
pawan <pawan.kumar@talentedge.in>
2018-05-17 10:55:33 +0530

    added new channels

--- custom/include/language/en_us.lang.php
+++ custom/include/language/en_us.lang.php
@@ -358,7 +358,9 @@ $GLOBALS['app_list_strings']['lead_source_custom_dom']=array (
   'OO_nitashi_noida'=>'Nitashi Noida',
   'OO_Skolege_Dwarka'=>'Skolege Dwarka',
   'OO_Akashline_Shahdara'=>'Akashline Shahdara',
-
+  'OO_c_le'=>'c_le',
+  'OO_c_la'=>'c_la',
+  'OO_c_us'=>'c_us',
 );
 $app_list_strings['moduleList']['te_ExpensePO']='Expense Vouchers';
 $app_list_strings['moduleListSingular']['te_ExpensePO']='Expense Vouchers';
