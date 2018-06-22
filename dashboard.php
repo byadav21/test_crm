03f755718a79f32abf028405f26c266180ba8cea
ashis mohanty <ashis.mohanty@talentedge.in>
2018-06-22 10:42:39 +0530

    batch work on dashboard

--- dashboard.php
+++ dashboard.php
@@ -1,4 +1,5 @@
 <?php
 if(!defined('sugarEntry'))define('sugarEntry', true);
 //require_once('lead_dashboard.php');
-require_once('lead_dashboard_cron.php');
+//require_once('lead_dashboard_cron.php');
+require_once('batch_dashboard.php');
