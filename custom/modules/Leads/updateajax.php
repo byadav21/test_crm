618a11b314e4ce6f2d84701ca6a5a168c6a3fe81
pawan <pawan.kumar@talentedge.in>
2018-09-26 18:03:54 +0530

    added filter on request for sanitize

--- custom/modules/Leads/updateajax.php
+++ custom/modules/Leads/updateajax.php
@@ -7,42 +7,46 @@ ini_set('display_errors', '1');
 error_reporting(E_ALL);
 global $app_list_strings, $current_user, $sugar_config, $db;
 
+$action = filter_var($_POST['action'], FILTER_SANITIZE_STRING);
+$param  = filter_var($_POST['param'], FILTER_SANITIZE_STRING);
+$leadid = filter_var($_POST['leadid'], FILTER_SANITIZE_STRING);
 
-if (isset($_POST['action']) && $_POST['action'] == 'updatenoteajax')
+if (isset($action) && $action == 'updatenoteajax')
 {
     global $db;
     $option = '';
-   
-    $param = $_POST['param']; $leadid = $_POST['leadid'];
-   
 
-    if ($param!='')
+    if ($param != '')
     {
-        $updateSql = "UPDATE leads SET note='".$param."' where id='$leadid'";
-        $usObj = $GLOBALS['db']->Query($updateSql);
-       
-        if($usObj){
-            echo json_encode(array('status'=>'success','msg'=>'Note saved successfully!'));
+
+        $note = mysqli_real_escape_string($db->database, $param);
+
+        $updateSql = "UPDATE leads SET note='" . $note . "' where id='$leadid'";
+        $usObj     = $GLOBALS['db']->Query($updateSql);
+
+        if ($usObj)
+        {
+            echo json_encode(array('status' => 'success', 'msg' => 'Note saved successfully!'));
         }
     }
     die;
 }
 
-if (isset($_POST['action']) && $_POST['action'] == 'updatecommentajax')
+if (isset($action) && $action == 'updatecommentajax')
 {
     global $db;
     $option = '';
-   
-    $param = $_POST['param']; $leadid = $_POST['leadid'];
-   
 
-    if ($param!='')
+    if ($param != '')
     {
-        $updateSql = "UPDATE leads SET comment='".$param."' where id='$leadid'";
-        $usObj = $GLOBALS['db']->Query($updateSql);
-       
-        if($usObj){
-            echo json_encode(array('status'=>'success','msg'=>'Comment saved successfully!'));
+        $comment = mysqli_real_escape_string($db->database, $param);
+
+        $updateSql = "UPDATE leads SET comment='" . $comment . "' where id='$leadid'";
+        $usObj     = $GLOBALS['db']->Query($updateSql);
+
+        if ($usObj)
+        {
+            echo json_encode(array('status' => 'success', 'msg' => 'Comment saved successfully!'));
         }
     }
     die;
