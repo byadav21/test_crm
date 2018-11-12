6ccf3ee001e9726ac4e937564f49b64393112a25
Pankaj <pankaj.jha@talentedge.in>
2018-11-12 15:01:27 +0000

    commented dashbaord crons

--- custom/modules/Leads/logic_hooks.php
+++ custom/modules/Leads/logic_hooks.php
@@ -20,5 +20,5 @@ $hook_array['after_retrieve'][] = Array(10, 'leads details', 'custom/modules/Lea
 $hook_array['process_record'][] = Array(6, 'statusoleads', 'custom/modules/Leads/lead_report.php','listviewlead', 'lead_report');
 $hook_array['after_relationship_add'][] = Array(7, 'abcd', 'custom/modules/Leads/refree_lead.php','Alogic', 'Bmethod');
 $hook_array['after_save'][] = Array(13, 'add leads primary vendor', 'custom/modules/Leads/save_primary_vendor.php','saveVendorClass','save_primary_vendor');
-$hook_array['after_save'][] = Array(14, 'add leads in report', 'custom/modules/Leads/add_leads_in_report_lead_tbl.php','saveLeadClass','add_lead');
+//$hook_array['after_save'][] = Array(14, 'add leads in report', 'custom/modules/Leads/add_leads_in_report_lead_tbl.php','saveLeadClass','add_lead');
 ?>
