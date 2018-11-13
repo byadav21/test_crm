9d92c576df00f7c9d0cf410b6e28a1afc14319bd
pawan <pawan.kumar@talentedge.in>
2018-11-13 14:13:08 +0530

    added columns for batch and student

---
+++ custom/Extension/modules/te_ba_Batch/Ext/Vardefs/sugarfield_batch_completion_date.php
@@ -0,0 +1,18 @@
+<?php
+$dictionary['te_ba_Batch']['fields']['batch_completion_date'] =array (
+	'name' => 'batch_completion_date',
+	'label' => 'LBL_BATCH_COMPLETION_DATE',
+	'type' => 'date',
+	'module' => 'te_ba_Batch',
+	'default_value' => '',
+	'help' => '',
+	'comment' => '',
+	'mass_update' => false, 
+	'required' => false, 
+	'reportable' => true, 
+	'audited' => false, 
+	'duplicate_merge' => false, 
+	'importable' => 'true', 
+);
+
+?>
