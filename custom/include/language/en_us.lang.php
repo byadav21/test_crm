f9ec2a3bfee2861424ec417a4669b47615297594
Pankaj <pankaj.jha@talentedge.in>
2018-12-10 14:43:31 +0000

    added new source CO_AK

--- custom/include/language/en_us.lang.php
+++ custom/include/language/en_us.lang.php
@@ -403,6 +403,7 @@ $GLOBALS['app_list_strings']['lead_source_custom_dom']=array (
   'CO_BLRTE'  => 'BLRTE',
   'CO_NK'=>'NK', 
   'CO_mark'=>'mark',
+  'CO_AK'=> 'AK',
 );
 $app_list_strings['moduleList']['te_ExpensePO']='Expense Vouchers';
 $app_list_strings['moduleListSingular']['te_ExpensePO']='Expense Vouchers';
