496befd78a74928efe3a95bf7f6729536ba1ef15
Pankaj <pankaj.jha@talentedge.in>
2019-01-15 18:12:52 +0530

    new te_focus facebook api token has been embedded

--- custom/modules/Leads/webhook.php
+++ custom/modules/Leads/webhook.php
@@ -69,8 +69,8 @@ if (!empty($input['entry'][0]['changes'][0]['value']['leadgen_id']))
     //createLog('{inside  empty check entry}', 'facebookleadCreated.txt', 'xx', $_REQUEST);
 
     $leadgen_id   = $input['entry'][0]['changes'][0]['value']['leadgen_id'];
-    //$access_token = 'EAAKcZB1mOFl4BAM31NoN0OZBpsRF7dBNNLzvnQ4HGSYeWXS3tXuZC3wjvKUSrZAWhaw5f4Y5STvkGCgyJUxXCTv9P7oLRqgYt3ZAyZAX41Y57dc2ONXlZA1s9ku8EBpFEOsMZBSiI8yWCu9suFPVCe2gI9TtZCZC8KNH8ZD';
-    $access_token = 'EAAKcZB1mOFl4BAE2haWnu4Po8r6vOOkPJkajx5K83WidCOIGIDtoppfNssWR13es9ae4fexZCZB8dhkui92kMBcF92mGzdMxATX6MNbuXQIldmvTGie3QEX79wt9pDVRdO5jhtOTYjQOlYXQmx6uj1fvbNhWZBcHR097Xgj3ZBAZDZD';
+    //$access_token = 'EAAKcZB1mOFl4BAE2haWnu4Po8r6vOOkPJkajx5K83WidCOIGIDtoppfNssWR13es9ae4fexZCZB8dhkui92kMBcF92mGzdMxATX6MNbuXQIldmvTGie3QEX79wt9pDVRdO5jhtOTYjQOlYXQmx6uj1fvbNhWZBcHR097Xgj3ZBAZDZD';
+    $access_token='EAAKcZB1mOFl4BAL54wR7plZCnYQ3ZBafG4JUb2klZCXKAoQWpD5PpZAHodZAwxQpzfV6BR0it3tdxJ502dqdnZCBYmK8jt5pR7PVJhlZAwjzNpJGhziiVFRSrEliGgNJqZBj13Yiv1NfGtDiCZAwHKVZAB4JeYx3oPCGJ0ZD';
     $leadurl      = $url . '/' . $leadgen_id . '?access_token=' . $access_token;
     $formId       = $input['entry'][0]['changes'][0]['value']['form_id'];
     $formData     = getFormName($formId, $url, $access_token);
