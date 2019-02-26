12c4c9deec3773d35f5d66d0c0c5c60d989fa426
pawan <pawan.kumar@talentedge.in>
2019-02-26 15:07:09 +0530

    added sap connection for slave and master

--- costum_import/db.php
+++ costum_import/db.php
@@ -1,7 +1,7 @@
 <?php
-# For 35.154.138.186
-//$conn=mysqli_connect("35.154.138.186","crm","crm","crm_te") or die("Could not connect");
-//mysqli_select_db($conn,"crm_te") or die("could not connect database");
+# For Staging CRM
+//$conn=mysqli_connect("crm-stage-env.cdftgd7ki47z.ap-south-1.rds.amazonaws.com","webuser","we8Us3r@543","crm_stage") or die("Could not connect");
+//mysqli_select_db($conn,"crm_stage") or die("could not connect database");
 
 # For Live CRM
 $conn=mysqli_connect("crm-db-server.cdftgd7ki47z.ap-south-1.rds.amazonaws.com","webuser","Us3r#&8)","crm_stage") or die("Could not connect");
--- costum_import/sap_db.php
+++ costum_import/sap_db.php
@@ -1,6 +1,6 @@
 <?php
-# For 35.154.138.186
-//$sap_conn=mysqli_connect("35.154.138.186","crm","crm","sap_crm") or die("Could not connect");
+# For Staging CRM
+//$sap_conn=mysqli_connect("crm-stage-env.cdftgd7ki47z.ap-south-1.rds.amazonaws.com","webuser","we8Us3r@543","crm_stage") or die("Could not connect");
 //mysqli_select_db($sap_conn,"sap_crm") or die("could not connect database");
 
 # For Live CRM
