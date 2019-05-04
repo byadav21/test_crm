a8624f6f05e71a257c2a7f83922e4a3b467dee5b
pawan <pawan.kumar@talentedge.in>
2019-05-04 23:24:25 +0530

    added simple curl

--- custom/modules/te_Api/delete_leadson_eloqua.php
+++ custom/modules/te_Api/delete_leadson_eloqua.php
@@ -2,10 +2,10 @@
 
 if (!defined('sugarEntry') || !sugarEntry)
     die('Not A Valid Entry Point');
-require_once('custom/modules/Leads/eloqua/lib/eloquaRequest.php');
+//require_once('custom/modules/Leads/eloqua/lib/eloquaRequest.php');
 
-//error_reporting(-1);
-//ini_set('display_errors', 'On');
+error_reporting(-1);
+ini_set('display_errors', 'On');
 
 class eloqua_contact
 {
@@ -26,7 +26,7 @@ class eloqua_contact
 
         //echo "<pre>"; print_r($bean);
 
-        $leadObj = $db->query("SELECT 
+        $lquery = "SELECT 
                                     lc.eloqua_contact_id,
                                     lc.eloqua_customobject_id,
                                     lc.eloqua_contact_status,
@@ -37,12 +37,12 @@ class eloqua_contact
                                AND (lc.eloqua_contact_id!=''
                                     OR lc.eloqua_customobject_id!='')
                                and lc.email_add_c like 'test%'
-                             LIMIT 10");
+                             LIMIT 10";
 
-        $result = $db->query($query);
+        $result = $db->query($lquery);
 
-        $contact_client = new EloquaRequest('https://secure.p07.eloqua.com/API/REST/1.0');
-        $custome_client = new EloquaRequest('https://secure.p07.eloqua.com/API/REST/2.0');
+        //$contact_client = new EloquaRequest('https://secure.p07.eloqua.com/API/REST/1.0');
+        //$custome_client = new EloquaRequest('https://secure.p07.eloqua.com/API/REST/2.0');
 
         if ($db->getRowCount($result) > 0)
         {
@@ -56,8 +56,37 @@ class eloqua_contact
 
                 if ($contact_id != '')
                 {
-                    $response = $contact_client->delete('/data/contact/' . $contact_id, '');
+                    //$response = $contact_client->delete('/data/contact/' . $contact_id, '');
+                   
 
+                    $curl = curl_init();
+
+                    curl_setopt_array($curl, array(
+                      CURLOPT_URL => "https://secure.p07.eloqua.com/api/REST/2.0/data/customObject/7/instance/987841",
+                      CURLOPT_RETURNTRANSFER => true,
+                      CURLOPT_ENCODING => "",
+                      CURLOPT_MAXREDIRS => 10,
+                      CURLOPT_TIMEOUT => 30,
+                      CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+                      CURLOPT_CUSTOMREQUEST => "DELETE",
+                      CURLOPT_HTTPHEADER => array(
+                        "authorization: Basic QXJyaW5hRWR1Y2F0aW9uU2VydmljZXNQcml2YXRlTGltaXRlZFxXZWJzaXRlLkFkbWluOlRlZGdlQDEyMw==",
+                        "cache-control: no-cache",
+                        "content-type: application/json"
+                      ),
+                    ));
+
+                    $response = curl_exec($curl);
+                    $err = curl_error($curl);
+
+                    curl_close($curl);
+
+                    if ($err) {
+                      echo "cURL Error #:" . $err;
+                    } else {
+                      echo $response;
+                    }
+                    
                     if ($response)
                     {
                         createLog('{In $contact_id}', 'delete_eloqua_log_' . date('Y-m-d') . '.txt', $contact_id, $response);
@@ -66,7 +95,7 @@ class eloqua_contact
 
                 if ($customobject_id != '')
                 {
-                    $response = $custome_client->delete('/data/customObject/7/instance/' . $customobject_id, '');
+                    //$response = $custome_client->delete('/data/customObject/7/instance/' . $customobject_id, '');
                     if ($response)
                     {
                         createLog('{In $customobject_id}', 'delete_eloqua_log_' . date('Y-m-d') . '.txt', $customobject_id, $response);
