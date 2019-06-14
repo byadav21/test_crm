c47ddefcb1a5a359efea5ce463abf00f61422c25
pawan <pawan.kumar@talentedge.in>
2019-06-14 15:12:07 +0530

    onclick on tab name for call log bug solved

--- custom/modules/Leads/views/view.detail.php
+++ custom/modules/Leads/views/view.detail.php
@@ -721,14 +721,19 @@ if(!isset($_SESSION['referral'])){
 		  });
 		}
                 
-                
-                $('#tab4').click(function(){
-                    var lead_id ='<?=$_REQUEST['record'];?>';
-                        $.ajax({url: "index.php?entryPoint=attemptlogger&action=getLogger&lead_id="+lead_id, success: function(result){
+                $(".yui-nav li").click(function() {
+                     var getTabName = $(this).find("a em").html();
+                     if(getTabName=='Call log')
+                     {
+                       var lead_id ='<?=$_REQUEST['record'];?>';
+                       $.ajax({url: "index.php?entryPoint=attemptlogger&action=getLogger&lead_id="+lead_id, success: function(result){
                                $('#list_subpanel_te_dispositionlogger_leads').html(result);
                         }});
-                    
+                       
+                     }
                 });
+                
+              
 	});
 
 
