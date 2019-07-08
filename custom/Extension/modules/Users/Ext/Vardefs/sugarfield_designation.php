661fba48519ba535624ab7cd1e92f11c30e94fa8
pankaj <pankaj@talentedge.in>
2019-07-08 12:14:26 +0530

    added and checked

--- custom/Extension/modules/Users/Ext/Vardefs/sugarfield_designation.php
+++ custom/Extension/modules/Users/Ext/Vardefs/sugarfield_designation.php
@@ -2,11 +2,22 @@
 $dictionary['User']['fields']['designation'] =array (
 	'name' => 'designation',
 	'vname' => 'LBL_DESIGNATION',
-	'required' => false,
+	'required' => true,
 	'type' => 'enum',
 	'importable' => 'true',
 	'len' => 50,
 	'size' => '20',
 	'options' => 'designation_list',
+);
+
+$dictionary['User']['fields']['department'] =array (
+        'name' => 'department',
+        'vname' => 'LBL_DEPARTMENT',
+        'required' => true,
+        'type' => 'enum',
+        'importable' => 'true',
+        'len' => 50,
+        'size' => '20',
+        'options' => 'department_list',
 );
 ?>
