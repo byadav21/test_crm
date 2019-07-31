16c663c1fa040cdada496cf740f9100de66f3daa
pawan <pawan.kumar@talentedge.in>
2019-07-31 17:23:49 +0530

    installment follow-up clicktocall wip v8

--- custom/modules/Leads/ajax.php
+++ custom/modules/Leads/ajax.php
@@ -16,15 +16,17 @@ if (isset($_POST['action']) && $_POST['action'] == 'seachLeadByMobileFollowup')
 
     $Email  = $_POST['Email'];
     $Mobile = $_POST['Mobile'];
-    
+
     $sqlConditions = '';
-    if(isset($Mobile) && $Mobile!=''){
-        
+    if (isset($Mobile) && $Mobile != '')
+    {
+
         $sqlConditions .= " AND leads.phone_mobile Like '%$Mobile%' ";
     }
-    
-    if(isset($Email) && $Email!=''){
-        
+
+    if (isset($Email) && $Email != '')
+    {
+
         $sqlConditions .= " AND lc.email_add_c  like '%$Email%' ";
     }
 
@@ -77,8 +79,13 @@ if (isset($_POST['action']) && $_POST['action'] == 'seachLeadByMobileFollowup')
             $phone              = $row['batch_code'];
             $phone_mobile       = $row['phone_mobile'];
             $table              .= "<tr><td>$userName</td><td>$status_description</td><td>$agentName</td><td>$batch_code</td><td>$phone_mobile</td>";
-            $table              .= '<td><img src="custom/themes/default/images/phone.png" href="" onclick="clickToCall('.$phone_mobile.', \''.$leadID.'\')" alt="" height="20" width="20"><td>';
+            $table              .= '<td><img src="custom/themes/default/images/phone.png" href="" onclick="clickToCall(' . $phone_mobile . ', \'' . $leadID . '\')" alt="" height="20" width="20"><td>';
             $table              .= "</tr>";
+        }
+
+        if ($db->getRowCount($usObj) == 0)
+        {
+            $table .= "<tr><td  align='center' style='border:1px solid red' colspan='6'>No Data Found!</td></td>";
         }
 
         $table .= '</table>';
@@ -86,4 +93,153 @@ if (isset($_POST['action']) && $_POST['action'] == 'seachLeadByMobileFollowup')
     }
     die;
 }
+
+
+if (isset($_POST['action']) && $_POST['action'] == 'seachLeadByDefaultSearch')
+{
+    global $db;
+    $option = '';
+    //RecordID: RecordID, RowID:RowID
+
+    $Email  = $_POST['email_id'];
+    $Mobile = $_POST['mobile_number'];
+    $Fname  = $_POST['first_name'];
+    $Lname  = $_POST['last_name'];
+    $lead_id= "";
+    //print_r($_REQUEST); die;
+    $where    = "";
+            $whereNew = '';
+            // condition add krna h
+//      name and email and mobile koi
+
+            if (!empty($Fname))
+            {
+                if ($where != '')
+                {
+                    $where .= " OR (tbl1.first_name like '%" . $Fname . "')";
+                }
+                else
+                {
+                    $where .= "  (tbl1.first_name like '%" . $Fname . "')";
+                }
+            }
+
+            if (!empty($lead_id))
+            {
+                if ($where != '')
+                {
+                    $where .= " OR (tbl1.id = '" . $lead_id . "')";
+                }
+                else
+                {
+                    $where .= "  (tbl1.id = '" . $lead_id . "')";
+                }
+            }
+
+            if (!empty($Lname))
+            {
+                if ($where != '')
+                {
+                    $where .= " OR (tbl1.last_name like '" . $Lname . "%')";
+                }
+                else
+                {
+                    $where .= "  (tbl1.last_name like '%" . $Lname . "%')";
+                }
+            }
+            if (!empty($Email))
+            {
+                if ($where != '')
+                {
+                    $where .= "  OR (tbl4.email_add_c like '" . $Email . "')";
+                }
+                else
+                {
+                    $where .= "  (tbl4.email_add_c like '" . $Email . "')";
+                }
+            }
+
+            if (!empty($Mobile))
+            {
+                if ($where != '')
+                {
+                    $where .= "  OR (tbl1.phone_mobile like '" . $Mobile . "')";
+                }
+                else
+                {
+                    $where .= "  (tbl1.phone_mobile like '" . $Mobile . "')";
+                }
+            }
+            if (strpos($where, 'tbl1.deleted') == false && $where != '')
+            {
+                $where .= " AND (tbl1.deleted=0) ";
+            }
+            if (!empty($where) && !empty($whereSRM))
+            {
+                $whereNew = " AND (tbl1.status = 'Converted')";
+            }
+
+
+            $fetch = "SELECT tbl1.id,
+                    tbl1.salutation,
+                    tbl1.phone_mobile,
+                    tbl4.email_add_c AS email_address,
+                    #tbl4.email_address,
+                    tbl1.assigned_user_id,
+                    tbl1.first_name,
+                    tbl1.last_name,
+                    tbl1.status,
+                    tbl1.status_description,
+                    tbl1.date_entered,
+                    tbl1.lead_source,
+                    tbl2.user_name,
+                    tbl2.phone_mobile,
+                    bb.batch_code
+                    
+             FROM leads AS tbl1
+             LEFT JOIN leads_cstm AS tbl4 ON tbl1.id= tbl4.id_c
+             LEFT JOIN users AS tbl2 ON tbl1.assigned_user_id = tbl2.id
+             LEFT join te_ba_batch bb on tbl4.te_ba_batch_id_c=bb.id and bb.deleted=0
+             WHERE  " . $where . " $whereNew
+             LIMIT 0, 100";
+            $usObj     = $db->query($fetch);
+            //echo $fetch;die();
+            
+        $vendor_Options = array();
+        $table          = '';
+        $table          .= '<table>';
+        $table          .= '<tr>
+                <th>Name</th>
+                <th>Status</th>
+                <th>Sub Status</th>
+                <th>Counsellor</th>               
+                <th>Batch</th>               
+                <th>Phone</th>  
+                </tr>';
+        while ($row            = $db->fetchByAssoc($usObj))
+        {
+            //print_r($row);
+            $leadID             = $row['id'];
+            $userName           = $row['first_name'] . ' ' . $row['last_name'];
+            $status             = $row['status'];
+            $status_description = $row['status_description'];
+            $agentName          = $row['user_name'];
+            $batch_code         = $row['batch_code'];
+            $phone              = $row['batch_code'];
+            $phone_mobile       = $row['phone_mobile'];
+            $table              .= "<tr><td>$userName</td><td>$status_description</td><td>$status</td><td>$agentName</td><td>$batch_code</td><td>$phone_mobile</td>";
+            //$table              .= '<td><img src="custom/themes/default/images/phone.png" href="" onclick="clickToCall(' . $phone_mobile . ', \'' . $leadID . '\')" alt="" height="20" width="20"><td>';
+            $table              .= "</tr>";
+        }
+
+        if ($db->getRowCount($usObj) == 0)
+        {
+            $table .= "<tr><td  align='center' style='border:1px solid red' colspan='6'>No Data Found!</td></td>";
+        }
+
+        $table .= '</table>';
+        echo $table;
+    }
+  
+
 ?>
--- custom/modules/Leads/search_leads.php
+++ custom/modules/Leads/search_leads.php
@@ -108,7 +108,7 @@ function createLog($action, $filename, $field = '', $dataArray = array())
             .slider.round:before {
                 border-radius: 50%;
             }
-            
+
             .block{display:flex; align-items: center; margin:20px 0}
             .block label + span{font-size: 14px; color:#4d4d4d; font-weight: bold;}
         </style>
@@ -122,7 +122,7 @@ function createLog($action, $filename, $field = '', $dataArray = array())
             </label>
             <span>Installment Follow-Up</span>
         </div>
-        
+
         <div id="installment_followup">
             <section class="moduleTitle">
                 <fieldset>
@@ -131,9 +131,9 @@ function createLog($action, $filename, $field = '', $dataArray = array())
                         <table width="100%">
                             <tr>
                                 <td><b>Email Name</b></td> 
-                                <td><input type="text" name="followup_email" id="followup_email"/></td>
+                                <td><input type="text" name="followup_email" id="followup_email" class="inputy" /></td>
                                 <td><b>Mobile Number</b></td> 
-                                <td><input type="text" name="followup_mobile_number" id="followup_mobile_number"/></td> 
+                                <td><input type="text" name="followup_mobile_number" id="followup_mobile_number" class="inputy" /></td> 
                                 <td><input type="Submit" name="Search_Converted_LeadX" value="Search Lead"></td> 
                                 </td></tr></br></br>
                         </table>       
@@ -146,22 +146,22 @@ function createLog($action, $filename, $field = '', $dataArray = array())
         <div id="default_search">
             <section class="moduleTitle">
                 <fieldset>
-                    <form action="index.php?module=Leads&action=search_leads&search_leads=1" method="post" id="lead_trans">
+                    <form action="" method="post" id="default_lead_trans">
 
                         <table width="100%">
                             <tr>
                                 <!---update code 5-dec-16 Manish Kumar-->
                                 <td><b>First Name</b></td> 
-                                <td><input type="text" name="name" id="name" class="inputx"/></td>
+                                <td><input type="text" name="first_name" id="first_name" class="inputx"/></td>
                                 <td><b>Last Name</b></td> 
-                                <td><input type="text" name="last" id="last" class="inputx"/></td>
+                                <td><input type="text" name="last_name" id="last_name" class="inputx"/></td>
                                 <td><b>Email Name</b></td> 
-                                <td><input type="text" name="email" id="email" class="inputx"/></td>
+                                <td><input type="text" name="email" id="email_id" class="inputx"/></td>
                                 <td><b>Mobile Number</b></td> 
-                                <td><input type="text" name="mobile_number" id="mobile_name" class="inputx"/></td> 
-                                <td><b>Lead ID</b></td> 
-                                <td><input type="text" name="lead_id" id="lead_id" class="inputx"/></td> 
-                                <td><input type="Submit" name="Search" value="Search Lead"></td> 
+                                <td><input type="text" name="mobile_number" id="mobile_numbery" class="inputx"/></td> 
+<!--                                <td><b>Lead ID</b></td> 
+                                <td><input type="text" name="lead_id" id="lead_id" class="inputx"/></td> -->
+                                <td><input type="Submit" name="Search_default_value" value="Search Lead"></td> 
 
                                 </td></tr></br></br>
                         </table>       
@@ -171,286 +171,96 @@ function createLog($action, $filename, $field = '', $dataArray = array())
             </section>
         </div>
         <br/>
-        <?php
-     
-
-        if (isset($_REQUEST['Search']))
-        {
-            extract($_REQUEST);
-            $where    = "";
-            $whereNew = '';
-            // condition add krna h
-//      name and email and mobile koi
-
-            if (!empty($name))
-            {
-                if ($where != '')
-                {
-                    $where .= " OR (tbl1.first_name like '%" . $name . "')";
-                }
-                else
-                {
-                    $where .= "  (tbl1.first_name like '%" . $name . "')";
-                }
-            }
-
-            if (!empty($lead_id))
-            {
-                if ($where != '')
-                {
-                    $where .= " OR (tbl1.id = '" . $lead_id . "')";
-                }
-                else
-                {
-                    $where .= "  (tbl1.id = '" . $lead_id . "')";
-                }
-            }
-
-            if (!empty($last))
-            {
-                if ($where != '')
-                {
-                    $where .= " OR (tbl1.last_name like '" . $last . "%')";
-                }
-                else
-                {
-                    $where .= "  (tbl1.last_name like '%" . $last . "%')";
-                }
-            }
-            if (!empty($email))
-            {
-                if ($where != '')
-                {
-                    $where .= "  OR (tbl4.email_add_c like '" . $email . "')";
-                }
-                else
-                {
-                    $where .= "  (tbl4.email_add_c like '" . $email . "')";
-                }
-            }
-
-            if (!empty($mobile_number))
-            {
-                if ($where != '')
-                {
-                    $where .= "  OR (tbl1.phone_mobile like '" . $mobile_number . "')";
-                }
-                else
-                {
-                    $where .= "  (tbl1.phone_mobile like '" . $mobile_number . "')";
-                }
-            }
-            if (strpos($where, 'tbl1.deleted') == false && $where != '')
-            {
-                $where .= " AND (tbl1.deleted=0) ";
-            }
-            if (!empty($where) && !empty($whereSRM))
-            {
-                $whereNew = " AND (tbl1.status = 'Converted')";
-            }
-
-
-            /*
-              $fetch = "SELECT tbl1.id,
-              tbl1.salutation,
-              tbl1.phone_mobile,
-              tbl4.email_address,
-              tbl1.assigned_user_id,
-              tbl1.first_name,
-              tbl1.last_name,
-              tbl1.status,
-              tbl1.date_entered,
-              tbl1.lead_source,
-              tbl2.user_name,
-              tbl2.phone_mobile
-              FROM leads AS tbl1
-              LEFT JOIN users AS tbl2 ON tbl1.assigned_user_id = tbl2.id
-              INNER JOIN email_addr_bean_rel AS tbl3 ON tbl1.id=tbl3.bean_id
-              INNER JOIN email_addresses AS tbl4 ON tbl3.email_address_id=tbl4.id
-              WHERE " . $where . " $whereNew
-              LIMIT 0, 100";
-             */
-            $fetch = "SELECT tbl1.id,
-       tbl1.salutation,
-       tbl1.phone_mobile,
-       tbl4.email_add_c AS email_address,
-       #tbl4.email_address,
-       tbl1.assigned_user_id,
-       tbl1.first_name,
-       tbl1.last_name,
-       tbl1.status,
-       tbl1.date_entered,
-       tbl1.lead_source,
-       tbl2.user_name,
-       tbl2.phone_mobile
-FROM leads AS tbl1
-LEFT JOIN leads_cstm AS tbl4 ON tbl1.id= tbl4.id_c
-LEFT JOIN users AS tbl2 ON tbl1.assigned_user_id = tbl2.id
-WHERE " . $where . " $whereNew
-             LIMIT 0, 100";
-
-            $row = $db->query($fetch);
-            
-            
-
-            //createLog('{createLog 1.}', 'search_leads_xx_'.date('Y-m-d').'.txt', $fetch,$row);
-            $cur_user='';
-           $countX = isset($row->num_rows)? $row->num_rows : 0 ;
-            if ($countX > 0)
-            {
-
-                $lead_ids = array();
-                while ($records  = $db->fetchByAssoc($row))
-                {
-                    $lead_ids[]    = $records['id'];
-                    $records_arr[] = $records;
-                    $cur_user      = $records['assigned_user_id'];
-                }
-            }
-            else
-            {
-                echo '<script>';
-                echo "alert('Search Leads Not Found')";
-                echo '</script>';
-                exit;
-            }
-
-
-            $_SESSION['records_fetch'] = $records_arr;
-            $_SESSION['leds_id']       = $lead_ids;
-        }
-
-
-        if (isset($_REQUEST['search_leads']) && $_REQUEST['search_leads'] == 1)
-        {   
-            $cur_user='';
-            echo '<div id="default_table_result"><table>';
-            echo ' <tr>
-                <th>Name</th>
-                <th>Status</th>
-                <th>Counsellor</th>               
-                <th>Batch</th>               
-                <th>Phone</th>               
-                </tr>';
-            if ($cur_user == $Us)
-            {
-                foreach ($_SESSION['records_fetch'] as $key => $value)
-                {
-                    ?>
-
-                <tr>
-                    <?php $linkid = $_SESSION['records_fetch'][$key]['id']; ?>
-                    <td><?php echo "<a href=index.php?action=ajaxui#ajaxUILoc=index.php%3Fmodule%3DLeads%26offset%3D1%26stamp%3DLeads%26action%3DDetailView%26record%3D$linkid" ?>> <?php echo $_SESSION['records_fetch'][$key]['salutation'] . $_SESSION['records_fetch'][$key]['first_name'] . '&nbsp;' . $_SESSION['records_fetch'][$key]['last_name']; ?></td>
-                    <td><?php echo $_SESSION['records_fetch'][$key]['status']; ?></td>
-                    <td><?php echo $_SESSION['records_fetch'][$key]['user_name']; ?></td>
-
-                    <?php
-                    $sql_ba = "SELECT b.id batch_id, b.name, l.id FROM leads l INNER JOIN leads_cstm lc ON l.id = lc.id_c AND l.id='" . $_SESSION['records_fetch'][$key]['id'] . "' LEFT JOIN te_utm ON l.utm = te_utm.name LEFT JOIN te_ba_batch b ON b.id = CASE WHEN l.utm =  'NA' THEN lc.te_ba_batch_id_c WHEN l.utm !=  'NA' THEN te_utm.te_ba_batch_id_c END";
-                    $res_ba = $GLOBALS['db']->query($sql_ba);
-                    $ba     = $GLOBALS['db']->fetchByAssoc($res_ba);
-
-                    $bid     = $ba['batch_id'];
-                    // Get programs details based on the Batch			
-                    $sql_pro = "SELECT te_pr_programs_te_ba_batch_1te_pr_programs_ida,name FROM te_pr_programs p INNER JOIN te_pr_programs_te_ba_batch_1_c  pb ON p.id = pb.te_pr_programs_te_ba_batch_1te_pr_programs_ida WHERE te_pr_programs_te_ba_batch_1te_ba_batch_idb = '{$bid}' AND pb.deleted = 0 AND p.deleted=0";
-
-                    $res_pro = $GLOBALS['db']->query($sql_pro);
-                    $pro     = $GLOBALS['db']->fetchByAssoc($res_pro);
-                    ?>
-                    <td width="50%"><?php echo $pro['name'] . ' -  ' . $ba['name']; ?></td>
-
-
-                    <td><?php echo $_SESSION['records_fetch'][$key]['phone_mobile']; ?></td>
-                </tr>
-                <?php
-            }
-        }
-        else
-        {
-
-            foreach ($_SESSION['records_fetch'] as $key => $value)
-            {
-                ?>
-
-                <tr>
-                    <?php $_SESSION['records_fetch'][$key]['id']; ?>
-                    <td><?php echo $_SESSION['records_fetch'][$key]['salutation'] . $_SESSION['records_fetch'][$key]['first_name'] . '&nbsp;' . $_SESSION['records_fetch'][$key]['last_name']; ?></td>
-                    <td><?php echo $_SESSION['records_fetch'][$key]['status']; ?></td>
-                    <td><?php echo $_SESSION['records_fetch'][$key]['user_name']; ?></td>
-
-
-                    <?php
-                    $sql_ba  = "SELECT b.id batch_id, b.name, l.id FROM leads l INNER JOIN leads_cstm lc ON l.id = lc.id_c AND l.id='" . $_SESSION['records_fetch'][$key]['id'] . "' LEFT JOIN te_utm ON l.utm = te_utm.name LEFT JOIN te_ba_batch b ON b.id = CASE WHEN l.utm =  'NA' THEN lc.te_ba_batch_id_c WHEN l.utm !=  'NA' THEN te_utm.te_ba_batch_id_c END";
-                    $res_ba  = $GLOBALS['db']->query($sql_ba);
-                    $ba      = $GLOBALS['db']->fetchByAssoc($res_ba);
-                    $bid     = $ba['batch_id'];
-                    // Get programs details based on the Batch			
-                    $sql_pro = "SELECT te_pr_programs_te_ba_batch_1te_pr_programs_ida,name FROM te_pr_programs p INNER JOIN te_pr_programs_te_ba_batch_1_c  pb ON p.id = pb.te_pr_programs_te_ba_batch_1te_pr_programs_ida WHERE te_pr_programs_te_ba_batch_1te_ba_batch_idb = '{$bid}' AND pb.deleted = 0 AND p.deleted=0";
-
-                    $res_pro = $GLOBALS['db']->query($sql_pro);
-                    $pro     = $GLOBALS['db']->fetchByAssoc($res_pro);
-                    ?>
-                    <td width="50%"><?php echo $pro['name'] . ' -  ' . $ba['name']; ?></td>
-
-                    <td><?php echo $_SESSION['records_fetch'][$key]['phone_mobile']; ?></td>
-                </tr>
-                <?php
-            }
-        }
-    }
-    echo '</table></div>';
-    ?>
-                <div id="showFollowupList"></div>
+        
+    <div id="showFollowupList"></div>
+    <div id="showDefaultList"></div>
 </body>
 
 <script>
-    var Usrx = "<?=$Us;?>";
+    var Usrx = "<?= $Us; ?>";
     $(document).ready(function () {
 
         $("#installment_followup").hide();
+
         
-           /*$("#lead_trans").on('submit', (function (e) {
-                    //class="inputx"
-                            event.preventDefault();
-                            var hasInput=false;
-                             $('.inputx').each(function () {
-                              if($(this).val()  !== ""){
-                               hasInput=true;
-                              }
-                             }); 
-                           
-                                if(!hasInput){
-                              alert("Need input!");
-                              
-                             }
-                             
-                             return true;
-                    console.log('test');
-                }));*/
-                
+        $("#default_lead_trans").on('submit', (function (e) {
 
+            //class="inputy"
+            event.preventDefault();
+            var hasInput = false;
+            $('.inputx').each(function () {
+                if ($(this).val() !== "") {
+                    hasInput = true;
+                }
+            });
+
+            if (!hasInput) {
+                alert("Need input!");
+                return false;
+            }
+
+            $.ajax({
+                beforeSend: function (request)
+                {
+                    SUGAR.ajaxUI.showLoadingPanel();
+                },
+                url: "index.php?entryPoint=accessleadajax",
+                data: {action: 'seachLeadByDefaultSearch', first_name: $("#first_name").val(), last_name: $("#last_name").val(),email_id: $("#email_id").val(),mobile_number: $("#mobile_numbery").val()},
+                dataType: "html",
+                type: "POST",
+                async: true,
+                success: function (data) {
+
+
+                    $("#showDefaultList").html(data);
+                    SUGAR.ajaxUI.hideLoadingPanel();
+
+                }
+            });
+
+
+
+
+            return false;
+        }));
+        
         $("#follow_up_leads").on('submit', (function (e) {
-            
-               $.ajax({
-                    beforeSend: function (request)
-                    {
-                        SUGAR.ajaxUI.showLoadingPanel();
-                    },
-                    url: "index.php?entryPoint=accessleadajax",
-                    data: {action: 'seachLeadByMobileFollowup', Email: $("#followup_email").val(), Mobile:$("#followup_mobile_number").val()},
-                    dataType: "html",
-                    type: "POST",
-                    async: true,
-                    success: function (data) {
-                      
-                      
-                        $("#showFollowupList").html(data);
-                          SUGAR.ajaxUI.hideLoadingPanel();
-                        
-                        }
-                    });
+
+            //class="inputy"
+            event.preventDefault();
+            var hasInput = false;
+            $('.inputy').each(function () {
+                if ($(this).val() !== "") {
+                    hasInput = true;
+                }
+            });
+
+            if (!hasInput) {
+                alert("Need input!");
+                return false;
+            }
+
+            $.ajax({
+                beforeSend: function (request)
+                {
+                    SUGAR.ajaxUI.showLoadingPanel();
+                },
+                url: "index.php?entryPoint=accessleadajax",
+                data: {action: 'seachLeadByMobileFollowup', Email: $("#followup_email").val(), Mobile: $("#followup_mobile_number").val()},
+                dataType: "html",
+                type: "POST",
+                async: true,
+                success: function (data) {
 
 
-           
+                    $("#showFollowupList").html(data);
+                    SUGAR.ajaxUI.hideLoadingPanel();
+
+                }
+            });
+
+
+
 
             return false;
         }));
@@ -460,38 +270,39 @@ WHERE " . $where . " $whereNew
 
         if ($("#openInstallmentForm").is(':checked') == true) {
             $("#default_search").hide();
-             $("#default_table_result").hide();
+            $("#default_table_result").hide();
             $("#installment_followup").show();
             $("#showFollowupList").show();
+             $("#showDefaultList").hide();
             
-            
+
+
 
         } else if ($("#openInstallmentForm").is(':checked') == false) {
             $("#default_search").show();
+            $("#showDefaultList").show();
             $("#installment_followup").hide();
-             $("#showFollowupList").hide();
-              $("#default_table_result").show();
-            
+            $("#showFollowupList").hide();
+            $("#default_table_result").show();
+
 
         }
     }
-    
-      function clickToCall(phone, lead_id) {
-                if (confirm('Are you sure to make a call')) {
-                    SUGAR.ajaxUI.showLoadingPanel();
-                    var callback = {
-                        success: function (b) {
-                            SUGAR.ajaxUI.hideLoadingPanel();
-                            if (b.responseText)
-                                swal(b.responseText);
-                        }
-                    }
-                    var connectionObject = YAHOO.util.Connect.asyncRequest('GET', 'index.php?entryPoint=clickToCall&lead=' + lead_id + '&number=' + phone + '&installment_followup_user=' + Usrx + '&installment_status=1', callback);
+
+    function clickToCall(phone, lead_id) {
+        if (confirm('Are you sure to make a call')) {
+            SUGAR.ajaxUI.showLoadingPanel();
+            var callback = {
+                success: function (b) {
+                    SUGAR.ajaxUI.hideLoadingPanel();
+                    if (b.responseText)
+                        swal(b.responseText);
                 }
             }
-            
-            
-           
+            var connectionObject = YAHOO.util.Connect.asyncRequest('GET', 'index.php?entryPoint=clickToCall&lead=' + lead_id + '&number=' + phone + '&installment_followup_user=' + Usrx + '&installment_status=1', callback);
+        }
+    }
+
 </script>
 
 </html>
