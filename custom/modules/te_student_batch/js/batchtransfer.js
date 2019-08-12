3ae766925cb5ef7a05c053f3e5baace30bd41871
pawankumar03 <ashis.mohanty@talentedge.in>
2019-08-12 16:00:07 +0530

    srm works

--- custom/modules/te_student_batch/js/batchtransfer.js
+++ custom/modules/te_student_batch/js/batchtransfer.js
@@ -69,7 +69,7 @@ app.controller("studentlisting", function($rootScope,$scope, $http) {
 			$scope.groupBatch=data.selbatch;
 			$scope.isprogramme=0;
 			$scope.req.cbid=data.currentbatchid;
-			
+			alert(data.result.name);
 			$('#studentModal').modal('show',{
 				backdrop: 'static',
 				keyboard: false,
