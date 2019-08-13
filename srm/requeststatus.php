d37f961786cfdc236117191ec5543a01b421b7c5
pawankumar03 <ashis.mohanty@talentedge.in>
2019-08-13 18:07:21 +0530

    srm approve page

---
+++ srm/requeststatus.php
@@ -0,0 +1,10 @@
+<?php
+if(!defined('sugarEntry'))define('sugarEntry', true);
+require_once('include/entryPoint.php');
+if($_GET['student_batch']!=''){
+	$student_batch	= $_GET['student_batch'];
+}else{
+	echo "Wrong URL";exit;
+}
+echo "<pre>";print_r($_REQUEST);echo "</pre>";
+?>
