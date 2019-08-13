ccb182a650aabf17534025b6c3311ff0e4bf5bac
pawankumar03 <ashis.mohanty@talentedge.in>
2019-08-13 18:24:42 +0530

    srm approve page

--- srmrequeststatus.php
+++ srmrequeststatus.php
@@ -4,7 +4,7 @@ require_once('include/entryPoint.php');
 if($_GET['student_batch']!=''){
 	$student_batch	= $_GET['student_batch'];
 }else{
-	echo "Wrong URL";exit;
+	echo "Wrong URL";exit; 
 }
 echo "<pre>";print_r($_REQUEST);echo "</pre>";
 ?>
