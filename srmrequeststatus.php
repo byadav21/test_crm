dcb5fb3910732436c76d2e6d3c11ab3792927516
pawankumar03 <ashis.mohanty@talentedge.in>
2019-08-23 17:51:40 +0530

    srm module

--- srmrequeststatus.php
+++ srmrequeststatus.php
@@ -23,7 +23,7 @@ if($_POST['Submit']){
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
     $result = curl_exec($ch);
     $res    = json_decode($result,TRUE);
-    //echo "=====<pre>";print_r($result);echo "</pre>";exit;
+    echo "=====<pre>";print_r($result);echo "</pre>";exit;
 	$updatedata="UPDATE te_student_batch set bt_fee_waiver='".$_POST['one']."', bt_approver_comments='".$_POST['approve_comment']."' where id='".$student_batch."'";
 	$updatequerydata=$db->query($updatedata);
 	//$updatestatus="UPDATE te_transfer_batch set status='".$_POST['two']."',is_new_approved=1, where batch_id_rel='".$student_batch."'";
