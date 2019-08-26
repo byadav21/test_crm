53010a97a4713dc90634d2a6447965cd313768e7
pawankumar03 <ashis.mohanty@talentedge.in>
2019-08-26 14:17:11 +0530

    srm work

--- custom/Extension/modules/te_student_batch/Ext/Vardefs/te_student_approval_status.php
+++ custom/Extension/modules/te_student_batch/Ext/Vardefs/te_student_approval_status.php
@@ -19,6 +19,15 @@ $dictionary['te_student_batch']['fields']['bt_approver_comments'] = array(
 	'massupdate' => false,	 
 	'studio' => 'visible',
 );
+$dictionary['te_student_batch']['fields']['bt_url'] = array(
+	'required' => false,
+	'name' => 'bt_url',
+	'vname' => 'bt_url',
+	'type' => 'text',
+	'audited' => false,
+	'massupdate' => false,	 
+	'studio' => 'visible',
+);
 
 
 
