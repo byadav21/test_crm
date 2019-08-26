c8beac0681767b0aaa01c318221d61efadf4db7c
pawankumar03 <ashis.mohanty@talentedge.in>
2019-08-26 14:11:43 +0530

    srm work

---
+++ custom/Extension/modules/te_student_batch/Ext/Vardefs/te_student_approval_status.php
@@ -0,0 +1,28 @@
+<?php
+$dictionary['te_student_batch']['fields']['bt_fee_waiver'] = array(
+	'required' => false,
+	'name' => 'bt_fee_waiver',
+	'vname' => 'bt_fee_waiver',
+	'type' => 'varchar',
+	'audited' => false,
+	'massupdate' => false,
+	'source' => 'non-db',
+	'studio' => 'visible',
+);
+
+$dictionary['te_student_batch']['fields']['bt_approver_comments'] = array(
+	'required' => false,
+	'name' => 'bt_approver_comments',
+	'vname' => 'bt_approver_comments',
+	'type' => 'text',
+	'audited' => false,
+	'massupdate' => false,	 
+	'studio' => 'visible',
+);
+
+
+
+
+
+
+
