0df3535b4c4a79d37b882e888a90f456b15b6524
pawankumar03 <ashis.mohanty@talentedge.in>
2019-08-29 13:21:25 +0530

    srm work

--- custom/modules/te_student_batch/student_batch.js
+++ custom/modules/te_student_batch/student_batch.js
@@ -44,7 +44,7 @@ $(document).ready(function () {
     if(feetype=='3'){
         msg='+ 5900 (Batch transfer fee)';
     }
-    $("#list_subpanel_te_student_batch_te_student_payment_plan_1>table>tbody").last('tr').after("<tr><th colspan='8' style='border: 1px solid #ddd;font-weight: bold;font-size: 1.5em;text-align: right;'>Pending Amount(Excluding Tax): "+pending_payment+msg+" Total Payment: " + total_payment + "</th></tr>");
+    $("#list_subpanel_te_student_batch_te_student_payment_plan_1>table>tbody").last('tr').after("<tr><th colspan='8' style='border: 1px solid #ddd;font-weight: bold;font-size: 1.5em;text-align: right;'>Pending Amount(Including Tax): "+pending_payment+msg+" Total Payment: " + total_payment + "</th></tr>");
 });
 
 
