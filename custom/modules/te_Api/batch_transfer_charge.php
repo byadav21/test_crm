785493be0490456678deddb8922b3adb1db43772
pawan <pawan.kumar@talentedge.in>
2019-09-11 13:02:43 +0530

    added a new BT api

---
+++ custom/modules/te_Api/batch_transfer_charge.php
@@ -0,0 +1,10 @@
+<?php
+
+if (!defined('sugarEntry') || !sugarEntry)
+    die('Not A Valid Entry Point');
+ini_set('memory_limit', '1024M');
+require_once('include/entryPoint.php');
+global $db;
+
+
+echo 'Hello world';
