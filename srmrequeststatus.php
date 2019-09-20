2a98f8cf8b6f524f7c8118092205b8a143d0ecd7
pawankumar03 <ashis.mohanty@talentedge.in>
2019-09-20 15:07:42 +0530

    crm work

--- srmrequeststatus.php
+++ srmrequeststatus.php
@@ -41,7 +41,8 @@ if($_POST['Submit'] && $error==0){
     curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($newdata));
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
    echo $resultarray = curl_exec($ch);
-    $res    = json_decode(strval($resultarray));
+   echo "-----".gettype($resultarray);
+    $res    = json_decode(key($resultarray), true);
     var_dump($res);
     echo "=====<pre>";print_r($res);echo "</pre>";exit;
 	$updatedata="UPDATE te_student_batch set bt_fee_waiver='".$_POST['one']."', bt_approver_comments='".$_POST['approve_comment']."', approve_status='".$_POST['two']."',batch_transfer_fee='".$btfee."' where id='".$student_batch."'";
