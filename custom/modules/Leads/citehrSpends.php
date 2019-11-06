84d76401dc8c4672d09e174afd1d232f954f85d2
pawan <pawan.kumar@talentedge.in>
2019-11-06 15:58:13 +0530

    added citeHR

--- custom/modules/Leads/citehrSpends.php
+++ custom/modules/Leads/citehrSpends.php
@@ -17,17 +17,12 @@ class citehrSpends
         $this->fromDate = date('Y-m-d');
         $this->toDate   = date('Y-m-d');
     }
-
-    public function get_data()
+    
+    public function getTotalC()
     {
         global $db;
         //$ignore_vendors = "'citehr','facebook','google','te_focus','taboola'";
-        $sql       = "SELECT count(id)total,
-                        sum(CASE
-                                WHEN status ='Converted' THEN 1
-                                ELSE 0
-                            END) AS converted,
-                        utm
+        $sql       = "SELECT count(id)total
                  FROM `leads`
                  WHERE date(date_entered)='" . $this->fromDate . "'
                    AND vendor LIKE '%citehr%' ";
@@ -35,57 +30,101 @@ class citehrSpends
         return $db->fetchByAssoc($result);
     }
 
+
+    public function get_data()
+    {
+        global $db;
+        //$ignore_vendors = "'citehr','facebook','google','te_focus','taboola'";
+        $sql       = "SELECT count(leads.id)total,
+                        sum(CASE
+                                WHEN leads.status ='Converted' THEN 1
+                                ELSE 0
+                            END) AS converted,
+                        leads.utm_term_c,
+                        leads_cstm.te_ba_batch_id_c
+                 FROM `leads`
+                 INNER JOIN leads_cstm ON leads.id= leads_cstm.id_c
+                 WHERE date(date_entered)='" . $this->fromDate . "'
+                   AND vendor LIKE '%citehr%' 
+                   group by utm_term_c ";
+        $result    = $db->query($sql);
+        $resultArr = [];
+        if ($db->getRowCount($result) > 0)
+        {
+            while ($row = $db->fetchByAssoc($result))
+            {
+                $resultArr[$row['utm_term_c']] = $row;
+            }
+        }
+        return $resultArr;
+    }
+
 }
 
-    $mainObj           = new citehrSpends();
-    $mainObj->fromDate = (isset($_GET['today']) && !empty($_GET['today'])) ? $_GET['today'] : date('Y-m-d', (strtotime('-1 day', strtotime(date('Y-m-d')))));
-    //$mainObj->fromDate = '2019-09-27';
-    
-    $citeData                = $mainObj->get_data();
-    $insertable_arr     = '';
-    $insertable_sub_arr = '';
+$mainObj           = new citehrSpends();
+$mainObj->fromDate = (isset($_GET['today']) && !empty($_GET['today'])) ? $_GET['today'] : date('Y-m-d', (strtotime('-1 day', strtotime(date('Y-m-d')))));
+//$mainObj->fromDate = '2019-09-27';
 
-    $citeHRAmt = 200000;
-    $vendorID= '14a4e733-b709-8c60-7731-590d5cb3b1f7';
-    echo '$maxDays=' . $maxDays   = date('t');
-    echo '$todayAmt=' . $todayAmt  = ($citeHRAmt / $maxDays);
+$citeData           = $mainObj->get_data();
+$insertable_arr     = '';
+$insertable_sub_arr = '';
 
-    $cpl_sum = $todayAmt;
-    $cpa     = ($cpl_sum > 0 && $citeData['converted']) ? $cpl_sum / $citeData['converted'] : 0;
-    $cpl     = ($cpl_sum > 0 && $citeData['total']) ? $cpl_sum / $citeData['total'] : 0;
-
-    $id               = create_guid();
-    $totalLeads       = $citeData['total'];
-    $totalConversion  = $citeData['converted'];
-    $ActualLead       = $citeData['total'];
-    $ActualConversion = $citeData['converted'];
-
-    $insertable_arr[]     = " ('" . $id . "','citehr','" . $mainObj->fromDate . "',$ActualLead,'" . $totalLeads . "','" . $ActualConversion . "','" . $totalConversion . "','" . $cpl_sum . "','" . $cpl . "','" . $cpa . "','','" . $vendorID . "','" . date('Y-m-d H:i:s') . "','" . date('Y-m-d H:i:s') . "') ";
-    $insertable_sub_arr[] = " ('" . create_guid() . "','','" . $id . "','" . date('Y-m-d H:i:s') . "') ";
+$citeHRAmt = 200000;
+$vendorID  = '14a4e733-b709-8c60-7731-590d5cb3b1f7';
+echo '$maxDays=' . $maxDays   = date('t');
+echo '$todayAmt=' . $todayAmt  = ($citeHRAmt / $maxDays);
 
 
+$totalData                = $mainObj->getTotalC();
 
-
-    global $db;
-    if ($insertable_arr)
+if ($result)
+{
+    foreach ($result as $key => $val)
     {
-        $insert_str = "INSERT INTO `te_actual_campaign`(`id`, `name`, `plan_date`,`leads`,`total_leads`,`actual_conversion`,`total_conversion`, `total_cost`, `cpl`, `cpa`, `te_ba_batch_id_c`, `vendor_id`,`date_entered`,`date_modified`) VALUES ";
-        $insert_str .= implode(',', $insertable_arr);
-        $db->query($insert_str);
-        //echo $insert_str.'<br>';
+
+       
+        $perLeadPrice   = $todayAmt / $totalData['total'];
+        $cpa            = ($cpl_sum > 0 && $val['converted']) ? $perLeadPrice / $val['converted'] : 0;
+        $cpl            = ($cpl_sum > 0 && $val['total']) ? $perLeadPrice * $val['total'] : 0;
+        $cpl_sum        = ($perLeadPrice * $val['total']);
+
+        $id               = create_guid();
+        $totalLeads       = $val['total'];
+        $totalConversion  = $val['converted'];
+        $ActualLead       = $val['total'];
+        $ActualConversion = $vals['converted'];
+
+        $insertable_arr[]     = " ('" . $id . "','citehr','" . $mainObj->fromDate . "',$ActualLead,'" . $totalLeads . "','" . $ActualConversion . "','" . $totalConversion . "','" . $cpl_sum . "','" . $cpl . "','" . $cpa . "','".$val['te_ba_batch_id_c']."','" . $vendorID . "','" . date('Y-m-d H:i:s') . "','" . date('Y-m-d H:i:s') . "') ";
+        $insertable_sub_arr[] = " ('" . create_guid() . "','','" . $id . "','" . date('Y-m-d H:i:s') . "') ";
     }
-    if ($insertable_sub_arr)
-    {
-        $insertable_sub_str = "INSERT INTO `te_utm_te_actual_campaign_1_c`(`id`, `te_utm_te_actual_campaign_1te_utm_ida`, `te_utm_te_actual_campaign_1te_actual_campaign_idb`,`date_modified`) VALUES ";
-        $insertable_sub_str .= implode(',', $insertable_sub_arr);
-        $db->query($insertable_sub_str);
-        //echo $insertable_sub_str;exit();
-    }
-    echo "<pre>";
-    print_r($insertable_arr);
-    print_r($insertable_sub_arr);
-    //print_r($result);
-    //print_r($utm);
-    exit();
+}//exit();
+
+
+
+
+
+
+
+global $db;
+if ($insertable_arr)
+{
+    $insert_str = "INSERT INTO `te_actual_campaign`(`id`, `name`, `plan_date`,`leads`,`total_leads`,`actual_conversion`,`total_conversion`, `total_cost`, `cpl`, `cpa`, `te_ba_batch_id_c`, `vendor_id`,`date_entered`,`date_modified`) VALUES ";
+    $insert_str .= implode(',', $insertable_arr);
+    //$db->query($insert_str);
+    //echo $insert_str.'<br>';
+}
+if ($insertable_sub_arr)
+{
+    $insertable_sub_str = "INSERT INTO `te_utm_te_actual_campaign_1_c`(`id`, `te_utm_te_actual_campaign_1te_utm_ida`, `te_utm_te_actual_campaign_1te_actual_campaign_idb`,`date_modified`) VALUES ";
+    $insertable_sub_str .= implode(',', $insertable_sub_arr);
+    //$db->query($insertable_sub_str);
+    //echo $insertable_sub_str;exit();
+}
+echo "<pre>";
+print_r($insertable_arr);
+print_r($insertable_sub_arr);
+//print_r($result);
+//print_r($utm);
+exit();
 
 
