0cf5da500c3d82c05fe804d2e90891acd50bc584
pawan <pawan.kumar@talentedge.in>
2020-02-25 16:02:56 +0530

    added required fields v2

--- custom/modules/Leads/push_leads_clevertap.php
+++ custom/modules/Leads/push_leads_clevertap.php
@@ -112,19 +112,19 @@ class pushLeadClevertap
         
         
         if($date_entered!=''){
-            $date_entered_doller = '$_';
+            $date_entered_doller = '$D_';
         }
         if($date_of_followup!=''){
-            $date_of_followup_doller = '$_';
+            $date_of_followup_doller = '$D_';
         }
         if($date_modified!=''){
-            $date_modified_doller = '$_';
+            $date_modified_doller = '$D_';
         }
         if($converted_date!=''){
-            $converted_date_doller = '$_';
+            $converted_date_doller = '$D_';
         }
         if($date_of_prospect!=''){
-            $date_of_prospect_doller = '$_';
+            $date_of_prospect_doller = '$D_';
         }
         # 2. event
 
@@ -141,23 +141,23 @@ class pushLeadClevertap
                         'Status_l'           => $status,
                         'status_description' => $status_description,
                         'date_entered'       => $date_entered,
-                        'date_entered_I'     => $date_entered_doller.''.strtotime($date_entered),
+                        'date_entered_I'     => $date_entered_doller.''.strtotime(gmdate('Y-m-d H:i:s', strtotime($date_entered))),
                         'date_of_followup'   => $date_of_followup,
-                        'date_of_followup_I' => $date_of_followup_doller.''.strtotime($date_of_followup),
+                        'date_of_followup_I' => $date_of_followup_doller.''.strtotime(gmdate('Y-m-d H:i:s', strtotime($date_of_followup))),
                         'date_modified'      => $date_modified,
-                        'date_modified_I'    => $date_modified_doller.''.strtotime($date_modified),
-                        'converted_date'     => $$converted_date,
+                        'date_modified_I'    => $date_modified_doller.''.strtotime(gmdate('Y-m-d H:i:s', strtotime($date_modified))),
+                        'converted_date'     => $converted_date,
                         'converted_date_I'   => $converted_date_doller.''.strtotime($converted_date),
                         'agent_id'           => $assigned_user_id,
                         'vendor'             => $vendor,
                         'batch_code'         => $batchCode,
                         'counsellor_name'    => $agent_name,
                         'date_of_prospect'   => $date_of_prospect,
-                        'date_of_prospect_I' => $date_of_prospect_doller.''.strtotime($date_of_prospect)
+                        'date_of_prospect_I' => $date_of_prospect_doller.''.strtotime(gmdate('Y-m-d H:i:s', strtotime($date_of_prospect)))
                     )
         )));
 
-        echo '<pre>';print_r($data);die;
+        //echo '<pre>';print_r($data);die;
         $payload = json_encode($data);
 
 
