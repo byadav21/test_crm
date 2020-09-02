2b7d6cf7c037efb723cd7199c4f59b47a23e5f06
brijesh <pawan.kumar@talentedge.in>
2020-09-02 14:15:38 +0530

    Sap API only state feilds data Updates

---
+++ costum_import/syncsaptables_cron_update_state.php
@@ -0,0 +1,111 @@
+<?php
+error_reporting(-1);
+ini_set('display_errors', 'On');
+
+#Invoices Header : Stud_OINV Update State  
+
+include 'db.php';
+include 'sap_db.php';
+
+class sync_update_tables
+{
+    function Stud_OINV()
+    {
+        global $conn;
+        $leadsCstmData    = array();
+        if(!empty($_REQUEST['startdate']) && !empty($_REQUEST['enddate'])){
+			$startDate 	= date("Y-m-d", strtotime($_REQUEST['startdate']));
+			$endDate 	= date("Y-m-d", strtotime($_REQUEST['enddate']));	
+			echo "StartDate:- ".$startDate." && EndDate:- ".$endDate."<br/>";
+		}
+        //die('imhere');
+       $query   = "SELECT `pd`.`state` AS `State`, `pd`.`invoice_order_number` AS `NumAtCard`
+                      FROM `te_payment_details` `pd` 
+                      WHERE `pd`.`deleted` = 0
+                        AND `pd`.`date_entered` > '$startDate' AND `pd`.`date_entered` <= '$endDate' 
+                      ORDER BY `pd`.`date_entered`";
+        $leadObj = mysqli_query($conn, $query);
+        if ($leadObj)
+        {
+			$i= 1;
+            while ($row = mysqli_fetch_assoc($leadObj))
+            {
+				//echo "<pre>==NumAtCard=="; print_r($row['NumAtCard']."==Address==".$row['Address']."==State==".$row['State']."====".$i);
+                $leadsCstmData[] = $row;
+                $i++;
+            }
+        }
+        
+        return $leadsCstmData;
+    }
+
+    function main()
+    {
+
+        global $sap_conn;
+
+        $Stud_OINVArr_new     = array();
+
+        #1. /////////// Stud_OINV Update State Table Syncing //////////////////
+        
+        $Stud_OINVArr_new = $this->Stud_OINV();
+        echo '<hr>Stud_OINV Table Syncing <br />';
+		
+        // Only State Feild Updates
+        $j = 1;
+        foreach ($Stud_OINVArr_new as $key => $set_data)
+        {
+			
+			$select_query = "SELECT State, NumAtCard from  `Stud_OINV` where NumAtCard = '".$set_data['NumAtCard']."' ";
+			$get_data 	= mysqli_query($sap_conn, $select_query) or die(mysqli_error($sap_conn));
+			if($get_data){
+				$row_count 	= mysqli_fetch_assoc($get_data);
+				
+				if(empty($row_count['State']) || ($row_count['State'] == NULL) ){
+					echo "Number of Count:- ".$j." && NumAtCard ID:- ".$set_data['NumAtCard']." && State:- ".$set_data['State']."<br/>";
+					$check_state = $set_data['State'];
+
+					switch ($check_state) {
+						case "BR":
+							$state = "BH";
+							break;
+						case "CT":
+							$state = "CG";
+							break;
+						case "DN":
+							$state = "DH";
+							break;
+						case "KA":
+							$state = "KT";
+							break;
+						case "ML":
+							$state = "ME";
+							break;
+						case "TG":
+							$state = "TS";
+							break;
+						case "UK":
+							$state = "UT";
+							break;
+						
+						default:
+							$state = $check_state;
+					}
+					$update_query = "UPDATE `Stud_OINV` SET `State` = '".$state."' where NumAtCard = '".$set_data['NumAtCard']."'  ";
+					$update_state = mysqli_query($sap_conn, $update_query) or die(mysqli_error($sap_conn));
+					$j++;
+				}
+			}
+			
+		}
+		
+        unset($Stud_OINVArr_new);
+    }
+
+// END of Main
+}
+
+$mainObj = new sync_update_tables();
+$mainObj->main();
+
+?>		 
--- sap_api.php
+++ sap_api.php
@@ -9,51 +9,51 @@ global $db;
 
 error_reporting(E_ALL);
 
-		if(!empty($_REQUEST['startdate']) && !empty($_REQUEST['enddate'])){
-			$startDate 	= date("Y-m-d", strtotime($_REQUEST['startdate']));
-			$endDate 	= date("Y-m-d", strtotime($_REQUEST['enddate']));	
-			echo $startDate."===".$endDate;
-			
-			$user = 'talentedgeadmin';
-			$password = 'Inkoniq@2016';
-			$url = $sugar_config['website_URL']."/crmordersync.php?startdate='".$startDate."'&enddate='".$endDate."'";
-			//$url = "http://site.talentedge.in/crmordersync.php?startdate='".$startDate."'&enddate='".$endDate."'";
-			
-			//echo $url;
-			$headers = array(
-					'Authorization: Basic '. base64_encode("$user:$password")
-			);
-			$post = [
-					'startdate' => $startDate,
-					'enddate' 	=> $endDate
-					
-			];
-			 
-			$ch = curl_init();
-			curl_setopt($ch, CURLOPT_URL,$url);
+	if(!empty($_REQUEST['startdate']) && !empty($_REQUEST['enddate'])){
+		$startDate 	= date("Y-m-d", strtotime($_REQUEST['startdate']));
+		$endDate 	= date("Y-m-d", strtotime($_REQUEST['enddate']));	
+		//echo $startDate."===".$endDate;
+		
+		$user = 'talentedgeadmin';
+		$password = 'Inkoniq@2016';
+		//$url = $sugar_config['website_URL']."/crmordersync.php?startdate='".$startDate."'&enddate='".$endDate."'";
+		$url = "http://site.talentedge.in/crmordersync.php?startdate='".$startDate."'&enddate='".$endDate."'";
+		
+		//echo $url;
+		$headers = array(
+				'Authorization: Basic '. base64_encode("$user:$password")
+		);
+		$post = [
+				'startdate' => $startDate,
+				'enddate' 	=> $endDate
+				
+		];
+		 
+		$ch = curl_init();
+		curl_setopt($ch, CURLOPT_URL,$url);
 
-			//curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-			curl_setopt($ch, CURLOPT_POST, 1);
-			curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($post));
-			curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
-			$result = curl_exec($ch);
-			$result = stripslashes(html_entity_decode($result));
-			$res = json_decode(trim($result),TRUE);
-			//echo "<pre>"; print_r($res);
-			$i=0;
+		//curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+		curl_setopt($ch, CURLOPT_POST, 1);
+		curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($post));
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+		$result = curl_exec($ch);
+		$result = stripslashes(html_entity_decode($result));
+		$res = json_decode(trim($result),TRUE);
+		//echo "<pre>"; print_r($res);
+		$i=0;
+		
+		foreach ($res as $value){
 			
-			foreach ($res as $value){
+			if( !empty($value['taxtype']) && !empty($value['state']) ){
 				
-				if( !empty($value['taxtype']) && !empty($value['state']) ){
-					
-					echo $query = "UPDATE te_payment_details SET tax_type='".$value['taxtype']."', state='".$value['state']."' where invoice_order_number='".$value['payment_id']."' ";
-					$qry1= $db->query($query);
-					echo "Numbers of Rows:- ".$i;
-					echo "</br>";
-				}
-			$i++;
+				$query = "UPDATE te_payment_details SET tax_type='".$value['taxtype']."', state='".$value['state']."' where invoice_order_number='".$value['payment_id']."' ";
+				$qry1= $db->query($query);
+				echo "Numbers of Rows:- ".$i;
+				echo "</br>";
 			}
-			
-				
+		$i++;
 		}
+		
+			
+	}
 ?>
