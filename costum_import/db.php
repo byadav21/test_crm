b7b2b9b16028a473da34d39ad2cffc6451bdef40
root <ashis.mohanty@talentedge.in>
2020-12-22 21:53:43 +0530

    sap

--- costum_import/db.php
+++ costum_import/db.php
@@ -1,9 +1,9 @@
 <?php
 # For Staging CRM
-$conn=mysqli_connect("crm-stage-env.cdftgd7ki47z.ap-south-1.rds.amazonaws.com","webuser","we8Us3r@543","crm_stage") or die("Could not connect");
-mysqli_select_db($conn,"crm_stage") or die("could not connect database");
+//$conn=mysqli_connect("crm-stage-env.cdftgd7ki47z.ap-south-1.rds.amazonaws.com","webuser","we8Us3r@543","crm_stage") or die("Could not connect");
+//mysqli_select_db($conn,"crm_stage") or die("could not connect database");
 
 # For Live CRM
-/*$conn=mysqli_connect("crm-db-server-v2.cdftgd7ki47z.ap-south-1.rds.amazonaws.com","webuser","Us3r#&8)","crm_stage") or die("Could not connect");
-mysqli_select_db($conn,"crm_stage") or die("could not connect database");*/
+$conn=mysqli_connect("crm-db-server-v2.cdftgd7ki47z.ap-south-1.rds.amazonaws.com","webuser","Us3r#&8)","crm_stage") or die("Could not connect");
+mysqli_select_db($conn,"crm_stage") or die("could not connect database");
 ?>
