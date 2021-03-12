93d1acf8f008431dfb858cbbb8b27b6d92a26395
brijesh.yadav <brijesh.kumar@talentedge.in>
2021-03-12 13:05:31 +0530

    add

--- costum_import/aymeo_delete_page.php
+++ costum_import/aymeo_delete_page.php
@@ -21,8 +21,8 @@ function createLog($action, $filename, $field = '', $dataArray = array())
 
 
 
-			$server = 'http://111.93.50.156:8888/ameyowebaccess/command/?command=force-login&data=';// live server
-            // $server = 'http://111.93.50.157:8888/ameyowebaccess/command/?command=force-login&data='; //Staging server
+			// $server = 'http://111.93.50.156:8888/ameyowebaccess/command/?command=force-login&data=';// live server
+            $server = 'http://111.93.50.157:8888/ameyowebaccess/command/?command=force-login&data='; //Staging server
 			
 			$data=[];
 			$data['userId']= 'pankaj.jha@talentedge.in';//$sugar_config['ameyo_import_login'];
@@ -36,14 +36,14 @@ function createLog($action, $filename, $field = '', $dataArray = array())
 			$session= curl_exec($ch); 
 			 
 			$jsonEncodedData = json_decode($session);
-            // createLog('{check installment follow up things}', 'aymeo_delete_check_api_'.date('Y-m-d').'.txt','', $session);
+            createLog('{check installment follow up things}', 'aymeo_delete_check_api_'.date('Y-m-d').'.txt','', $session);
             echo "imhere";
-            echo "<pre>";print_r($jsonEncodedData->sessionId);//die('imhere1234568');ok
+            echo "<pre>";print_r($jsonEncodedData->sessionId);//die('imhere1234568');
 
         // Delete API {"campaignId":"1","sessionId":"abc-133123-xyz","customerIds":[123456,123456,123456]}
         
-        $campaignId                        = '16';
-        $customerIds                       = '16788768';
+        // $campaignId                        = '16';
+        // $customerIds                       = '16788768';
         // $api                               = new te_Api_override();
         $data                              = [];
         $sessionId                           = $jsonEncodedData->sessionId;
@@ -54,18 +54,15 @@ function createLog($action, $filename, $field = '', $dataArray = array())
         
             // echo "brijesh";
             $request = [];
-            $request['campaignId']	= '49';//'16';//($campaignId)? $campaignId :$sugar_config['ameyo_campaigainID'];
+            $request['campaignId']	= '16';//49/''
 			$request['sessionId'] 	= $sessionId;
-			$request['customerIds']	= [88126854,88126853,88126852];//'16788768';//($customerIds)? $customerIds : $sugar_config['ameyo_customerIds'];	
-			 $data_url = 'http://111.93.50.156:8888/ameyowebaccess/command?command=removeContactsFromCampaign&data='; // Live server
-            //  $data_url = 'http://111.93.50.157:8888/ameyowebaccess/command?command=removeContactsFromCampaign&data=';//Staging server
+			$request['customerIds']	= ['19154906'];//,88126853,88126852,'16788768';//($customerIds)? $customerIds : $sugar_config['ameyo_customerIds'];	
+			//  $data_url = 'http://111.93.50.156:8888/ameyowebaccess/command?command=removeContactsFromCampaign&data='; // Live server
+             $data_url = 'http://111.93.50.157:8888/ameyowebaccess/command?command=removeContactsFromCampaign&data=';//Staging server
             $url = $data_url. (json_encode($request));
             echo "<br />";print_r($url);
-            //http://111.93.50.156:8888/ameyowebaccess/command?command=removeContactsFromCampaign&data={"campaignId":"49","sessionId":"d426-60478059-ses-pankaj.jha@talentedge.in-0tBiw35D-5274","customerIds":[88126854,88126853,88126852]}Rachit 
-
-
-            //http://192.168.1.230:8888/ameyowebaccess/command/?command=removeContactsFromCampaign&data={"campaignId":"1","sessionId":"abc-133123-xyz","customerIds":[123456,123456,123456]}
-			$ch = curl_init(); 
+            
+            $ch = curl_init(); 
             curl_setopt($ch, CURLOPT_URL, $url);
 			curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 			curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 10);
@@ -73,17 +70,11 @@ function createLog($action, $filename, $field = '', $dataArray = array())
 			curl_setopt($ch, CURLOPT_POST, true);
 			// curl_setopt($ch, CURLOPT_POSTFIELDS, "data=".urlencode(json_encode($request)));					
 			$response = curl_exec($ch);
-            // createLog('{check installment follow up things}', 'aymeo_delete_api_'.date('Y-m-d').'.txt','', $response);
+            createLog('{check installment follow up things}', 'aymeo_delete_api_'.date('Y-m-d').'.txt','', $response);
 			// $this->createLog(print_r($data,true),$response,$data);	
 		   // $response= file_get_contents($server. urlencode(json_encode($request)));			
-           echo "Rachit <pre>"; print_r($request);	 
-           print_r($response);	 
            $responses=json_decode($response);		
-           echo "bky <pre>"; //print_r($request);	 
-           print_r($response);		
-			// return $responses;
+        	// return $responses;
         
-
-        echo "123:- <pre>";print_r($responses);
         exit('imhere the end');
    
